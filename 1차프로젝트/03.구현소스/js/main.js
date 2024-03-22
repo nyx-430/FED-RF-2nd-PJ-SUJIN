b013ba98e41c99a37a78a064ec2dd55f41e14eb1
Sujin <9_ight@naver.com>
2024-03-22 18:18:36 +0900

    0322/18:18

---
+++ 1차프로젝트/03.구현소스/js/main.js
@@ -0,0 +1,10 @@
+// Beanpole 메인 JS //
+
+document.addEventListener('DOMContentLoaded', loadFn);
+
+function loadFn(){
+    console.log('로딩 완료!');
+
+   /*  const slider=document.querySelectorAll('.slider'); */
+
+} /////////////// loadFn 함수 ///////////////
