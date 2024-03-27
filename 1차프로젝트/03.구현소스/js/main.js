ae429dde491600ba8e8c1625f3879b48c4ebd6fd
Sujin <9_ight@naver.com>
2024-03-27 18:10:38 +0900

    0327/18:10

--- 1차프로젝트/03.구현소스/js/main.js
+++ 1차프로젝트/03.구현소스/js/main.js
@@ -3,9 +3,25 @@
 let pgNum=0;
 let stsWheels=false;
 
-const elePage=document.querySelectorAll(.)
+const elePage=document.querySelector('.page');
+const totalCnt=elePage.length;
 
-/* document.querySelector("html").style.scrollBehavior = "smooth";
+console.log('메인페이지:',elePage);
+
+window.addEventListener("wheel",wheelFn,{passive:false});
+
+function wheelFn(e){
+  console.log('휠이 안 됨!');
+
+  e.preventDefault();
+
+
+}; ///////// wheelFn /////////
+/////////////////////////////
+
+
+// 8초 후 아래로 이동
+document.querySelector("html").style.scrollBehavior = "smooth";
 setTimeout(() => {
   window.scrollTo(0, document.querySelector("#ban-area").offsetTop);
-}, 8000); */
+}, 8000);
