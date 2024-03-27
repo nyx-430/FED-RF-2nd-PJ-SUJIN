0f14cd3abb960c98551cafdfb7781e254fa6ea16
Sujin <9_ight@naver.com>
2024-03-27 13:20:16 +0900

    0327/13:20

--- 1차프로젝트/03.구현소스/js/main.js
+++ 1차프로젝트/03.구현소스/js/main.js
@@ -1,18 +1,7 @@
 // Beanpole 메인 JS //
 
+document.querySelector("html").style.scrollBehavior = "smooth";
+setTimeout(() => {
+  window.scrollTo(0, document.querySelector("#ban-area").offsetTop);
+}, 8000);
 
-
-/* document.querySelector('html').style.scrollBehavior = 'smooth';
-      setTimeout(() => {
-        window.scrollTo(0,document.querySelector('#ban-area').offsetTop);
-      }, 8000); */
-
-function loadFn(){
-
-console.log('로딩 완료');
-
-
-
-
-}; /////////// loadFn 함수 ///////////
-/////////////////////////////////////
