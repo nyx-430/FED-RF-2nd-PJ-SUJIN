83eef34e5815c44b97015c8d1f8836697cd8d9de
Sujin <9_ight@naver.com>
2024-03-28 18:19:42 +0900

    0328/18:19

--- 1차프로젝트/03.구현소스/js/main.js
+++ 1차프로젝트/03.구현소스/js/main.js
@@ -1,13 +1,27 @@
 // Beanpole 메인 JS //
 
+// 8초 후 아래로 이동
+document.querySelector("html").style.scrollBehavior = "smooth";
+setTimeout(() => {
+  window.scrollTo(0, document.querySelector("#ban-area").offsetTop);
+}, 8000);
+
+// 휠 이벤트 - x
+// 페이지 변수
 let pgNum=0;
+
+// 휠 상태 변수
 let stsWheels=false;
 
-const elePage=document.querySelector('.page');
+// .page 클래스 요소
+// const elePage=document.querySelector('.page');
+
+// 전체 페이지수
 const totalCnt=elePage.length;
 
-console.log('메인페이지:',elePage);
+console.log('대상:',elePage,totalCnt);
 
+// 이벤트 등록
 window.addEventListener("wheel",wheelFn,{passive:false});
 
 function wheelFn(e){
@@ -15,13 +29,9 @@ function wheelFn(e){
 
   e.preventDefault();
 
+}; //////////// wheelFn ////////////
+///////////////////////////////////
 
-}; ///////// wheelFn /////////
-/////////////////////////////
 
 
-// 8초 후 아래로 이동
-document.querySelector("html").style.scrollBehavior = "smooth";
-setTimeout(() => {
-  window.scrollTo(0, document.querySelector("#ban-area").offsetTop);
-}, 8000);
+
