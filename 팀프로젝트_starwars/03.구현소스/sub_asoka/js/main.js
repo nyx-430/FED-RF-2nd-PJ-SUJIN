61e27332066a42ed709bd07f0880bfb1601db5f4
Sujin <9_ight@naver.com>
2024-04-30 18:13:17 +0900

    0430/18:13

--- 팀프로젝트_starwars/03.구현소스/sub_asoka/js/main.js
+++ 팀프로젝트_starwars/03.구현소스/sub_asoka/js/main.js
@@ -2,3 +2,52 @@
 
 import mFn from "./my_function.js";
 
+//console.log(mFn);
+const stage=mFn.qsa('.page');
+const scAct=mFn.qsa('.scAct');
+console.log('페이지:',stage,'\n scAct박스:',scAct);
+
+const pagePos = [];
+stage.forEach((ele,idx)=>{
+  pagePos[idx] = ele.offsetTop;
+})
+console.log('페이지 높이값:',pagePos);
+
+//const CRITERIA=window.innerHeight/2;
+
+// let pos=getBCR;
+// 
+// function scrollFn(x){
+//     let xval=pos(x);
+// 
+//     if(xval < CRITERIA && xval > 0){
+//         x.classList.add("on");
+//     } /// if ///
+// } /////////// scrollFn ///////////
+// 
+// window.addEvt("scroll",()=>{
+//     for(let x of scAct) scrollFn(x);
+// });
+
+mFn.addEvt(window,'scroll',scrollFn);
+
+const CRITERIA=window.innerHeight/2
+
+function scrollFn(){
+
+    stage.forEach((ele,idx)=>{
+        // 대상의 BCR값 알아오기
+        let pos=mFn.getBCR(ele);
+    
+        console.log('스크롤~',pos);
+    
+        // 기준값보다 작아지면 on 넣기
+        if(pos < CRITERIA){
+            scAct[idx].classList.add('on');
+        } 
+        else{
+            scAct[idx].classList.remove('on');
+        }
+
+    })
+} ///////////// scrollFn /////////////
