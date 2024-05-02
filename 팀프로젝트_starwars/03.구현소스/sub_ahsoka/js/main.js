95d17f6333132ca3829e199a5719ac9bbf3a51bf
Sujin <9_ight@naver.com>
2024-05-02 15:21:21 +0900

    0502/15:21

--- 팀프로젝트_starwars/03.구현소스/sub_ahsoka/js/main.js
+++ 팀프로젝트_starwars/03.구현소스/sub_ahsoka/js/main.js
@@ -2,36 +2,67 @@
 
 import mFn from "./my_function.js";
 
-//console.log(mFn);
-const stage=mFn.qsa('.page');
-const scAct=mFn.qsa('.scAct');
-console.log('페이지:',stage,'\n scAct박스:',scAct);
+// 비디오 영역 아이프레임 src 변경
+const slidePeek = mFn.qsa(".slide-peek-box a");
+const ifr = mFn.qs("#ifr");
+
+console.log("슬라이드 픽:", slidePeek, "\n 동영상:", ifr);
+
+const movieId = {
+  Trailer: "J_1EXWNETiI",
+  "Masters and Apprentices": "UTqPkjk0GLU",
+  Phenomenon: "dp-9AoWxchE",
+  "Dual Lightsaber": "ZQnGCtQ0N8Q",
+};
+
+slidePeek.forEach((ele, idx) => {
+  ele.onclick = () => {
+    let txt = ele.querySelector("img").getAttribute("alt");
+    console.log("나 클릭!", ele);
+
+    ifr.setAttribute(
+      "src",
+      `https://www.youtube.com/embed/${movieId[txt]}?autoplay=1`
+    );
+
+    forEach((x, i) => {
+      if (i === idx) {
+        x.parentElement.classList.add("on");
+      } /// if ///
+      else {
+        x.parentElement.classList.remove("on");
+      } /// else ///
+    }); /// forEach ///
+  }; /// onclick ///
+}); ////// forEach //////
+
+// 스크롤 페이드인/아웃
+const stage = mFn.qsa(".page");
+const scAct = mFn.qsa(".scAct");
+console.log("페이지:", stage, "\n scAct박스:", scAct);
 
 const pagePos = [];
-stage.forEach((ele,idx)=>{
+stage.forEach((ele, idx) => {
   pagePos[idx] = ele.offsetTop;
-})
-console.log('페이지 높이값:',pagePos);
+});
+console.log("페이지 높이값:", pagePos);
 
-mFn.addEvt(window,'scroll',scrollFn);
+mFn.addEvt(window, "scroll", scrollFn);
 
-const CRITERIA=window.innerHeight/3*2
+const CRITERIA = (window.innerHeight / 3) * 2;
 
-function scrollFn(){
+function scrollFn() {
+  stage.forEach((ele, idx) => {
+    // 대상의 BCR값 알아오기
+    let pos = mFn.getBCR(ele);
 
-    stage.forEach((ele,idx)=>{
-        // 대상의 BCR값 알아오기
-        let pos=mFn.getBCR(ele);
-    
-        // console.log('스크롤~',pos);
-    
-        // 기준값보다 작아지면 on 넣기
-        if(pos < CRITERIA){
-            scAct[idx].classList.add('on');
-        } 
-        else{
-            scAct[idx].classList.remove('on');
-        }
+    // console.log('스크롤~',pos);
 
-    })
+    // 기준값보다 작아지면 on 넣기
+    if (pos < CRITERIA) {
+      scAct[idx].classList.add("on");
+    } else {
+      scAct[idx].classList.remove("on");
+    }
+  });
 } ///////////// scrollFn /////////////
