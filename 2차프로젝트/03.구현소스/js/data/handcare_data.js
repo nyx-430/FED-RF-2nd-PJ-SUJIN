1127d77ff0b738ecf9b562be28d1834f560e3237
Sujin <9_ight@naver.com>
2024-06-11 10:06:58 +0900

    Create handcare_data.js

---
+++ 2차프로젝트/03.구현소스/js/data/handcare_data.js
@@ -0,0 +1,236 @@
+// SW19 상품 데이터
+
+// 핸드케어 데이터
+
+export const handData = [
+  {
+    idx: 1,
+    name: "SW19 6am HAND CREAM (50ml)",
+    category: "Hand Care",
+    price: "23,000",
+    desc: `The SW19 perfume-scented hand cream is enriched with silky powder and mild botanical moisturizing ingredients to nourish and moisturize hands without sticky after-feel.
+        
+    SW19 퍼퓸 향을 그대로 담은 핸드크림은 실키 파우더가 함유되어 끈적임 없이 매끄럽게 피부결을 정돈하며, 비건 포뮬러로 만들어진 순한 식물성 보습 성분이 피부를 산뜻하고 건강하게 유지합니다.
+    
+    Ingredients certified by the EVE VEGAN® MARK
+    ※ Plant based 원료 특성상 갈색의 침전물이 발생할 수 있으며, 이는 자연 현상으로 제품 사용에는 문제가 없음을 미리 알려드립니다.`,
+    top: "Bergamot, Grapefruit, Anise Seeds",
+    heart: "Cardamom, Clary Sage, Basil",
+    base: "Wormwood, Cedarwood, Musk",
+    perfumer: "Aliénor Massenet",
+    ingredients:
+      "Water, Glycerin, Butylene Glycol, Butyrospermum Parkii (Shea) Butter, Cetearyl Alcohol, Hydrogenated Polydecene, Fragrance(Parfum), 1,2-Hexanediol, Caprylic/Capric Triglyceride, Macadamia Integrifolia Seed Oil, Chlorella Vulgaris Extract, Melia Azadirachta Flower Extract, Ocimum Sanctum Leaf Extract, Melia Azadirachta Leaf Extract, Curcuma Longa (Turmeric) Root Extract, Corallina Officinalis Extract, Xylitylglucoside, Centella Asiatica Leaf Extract, Centella Asiatica Extract, Xylitol, Sodium Hyaluronate, Hydrolyzed Hyaluronic Acid, Hyaluronic Acid, Polyglyceryl-3 Methylglucose Distearate, Synthetic Beeswax, Hydrogenated Lecithin, Ammonium Acryloyldimethyltaurate/VP Copolymer, Silica, Vinyl Dimethicone, Hydroxyethyl Acrylate/Sodium Acryloyldimethyl Taurate Copolymer, Glyceryl Stearate, Caprylyl Glycol, Glucose, Fructooligosaccharides, Fructose, Acrylates/C10-30 Alkyl Acrylate Crosspolymer, Ethylhexylglycerin, Tromethamine, Sodium Phytate, Tocopherol, Madecassoside, Anhydroxylitol, Asiaticoside, Ceramide NP, Hydroxypropyltrimonium Hyaluronate, Sodium Acetylated Hyaluronate, Sodium Stearoyl Glutamate, Hydrolyzed Sodium Hyaluronate, Sodium Hyaluronate Crosspolymer, Potassium Hyaluronate, Cetyl Alcohol, Xanthan Gum, Linalool, Limonene, Coumarin, Eugenol, Hydroxycitronellal, Geraniol",
+  },
+  {
+    idx: 2,
+    name: "SW19 Noon HAND CREAM (50ml)",
+    category: "Hand Care",
+    price: "23,000",
+    desc: `The SW19 perfume-scented hand cream is enriched with silky powder and mild botanical moisturizing ingredients to nourish and moisturize hands without sticky after-feel.
+
+    SW19 퍼퓸 향을 그대로 담은 핸드크림은 실키 파우더가 함유되어 끈적임 없이 매끄럽게 피부결을 정돈하며, 비건 포뮬러로 만들어진 순한 식물성 보습 성분이 피부를 산뜻하고 건강하게 유지합니다.
+
+    Ingredients certified by the EVE VEGAN® MARK`,
+    top: "Green Leaf, Aqua, Mint, Almond Blossom",
+    heart: "Vetiver, Lavender, Lily of The Vally",
+    base: "Orris(Iris), Cedar Wood, Amber, Musk",
+    perfumer: "Marianne Nawrocki Sabatier",
+    ingredients: "Water, Glycerin, Butylene Glycol, Butyrospermum Parkii (Shea) Butter, Cetearyl Alcohol, Hydrogenated Polydecene, Fragrance(Parfum), 1,2-Hexanediol, Caprylic/Capric Triglyceride, Macadamia Integrifolia Seed Oil, Chlorella Vulgaris Extract, Melia Azadirachta Flower Extract, Ocimum Sanctum Leaf Extract, Melia Azadirachta Leaf Extract, Curcuma Longa (Turmeric) Root Extract, Corallina Officinalis Extract, Xylitylglucoside, Centella Asiatica Leaf Extract, Xylitol, Centella Asiatica Extract, Sodium Hyaluronate, Hydrolyzed Hyaluronic Acid, Hyaluronic Acid, Polyglyceryl-3 Methylglucose Distearate, Synthetic Beeswax, Hydrogenated Lecithin, Ammonium Acryloyldimethyltaurate/VP Copolymer, Silica, Vinyl Dimethicone, Hydroxyethyl Acrylate/Sodium Acryloyldimethyl Taurate Copolymer, Glyceryl Stearate, Caprylyl Glycol, Glucose, Fructooligosaccharides, Fructose, Ethylhexylglycerin, Acrylates/C10-30 Alkyl Acrylate Crosspolymer, Tromethamine, Sodium Phytate, Beta-Glucan, Tocopherol, Madecassoside, Anhydroxylitol, Asiaticoside, Ceramide NP, Hydroxypropyltrimonium Hyaluronate, Sodium Acetylated Hyaluronate, Sodium Stearoyl Glutamate, Hydrolyzed Sodium Hyaluronate, Sodium Hyaluronate Crosspolymer, Potassium Hyaluronate, Cetyl Alcohol, Xanthan Gum, Linalool, Limonene, Hydroxycitronellal, Coumarin, Citronellol, Geraniol, Alpha-Isomethyl Ionone",
+  },
+  {
+    idx: 3,
+    name: "SW19 3pm HAND CREAM (50ml)",
+    category: "Hand Care",
+    price: "23,000",
+    desc: `The SW19 perfume-scented hand cream is enriched with silky powder and mild botanical moisturizing ingredients to nourish and moisturize hands without sticky after-feel.
+
+    SW19 퍼퓸 향을 그대로 담은 핸드크림은 실키 파우더가 함유되어 끈적임 없이 매끄럽게 피부결을 정돈하며, 비건 포뮬러로 만들어진 순한 식물성 보습 성분이 피부를 산뜻하고 건강하게 유지합니다.
+
+    Ingredients certified by the EVE VEGAN® MARK`,
+    top: "Bergamot, Rhubarb, Tangerine Lotus",
+    heart: "Cedarwood, Jasmine, Gardenia",
+    base: "White Musk, Vetiver, Orris",
+    perfumer: "Nathalie Benareau",
+    ingredients: "Water, Glycerin, Butylene Glycol, Butyrospermum Parkii (Shea) Butter, Cetearyl Alcohol, Hydrogenated Polydecene, Fragrance(Parfum), 1,2-Hexanediol, Caprylic/Capric Triglyceride, Macadamia Integrifolia Seed Oil, Chlorella Vulgaris Extract, Melia Azadirachta Flower Extract, Ocimum Sanctum Leaf Extract, Melia Azadirachta Leaf Extract, Curcuma Longa (Turmeric) Root Extract, Corallina Officinalis Extract, Xylitylglucoside, Centella Asiatica Leaf Extract, Centella Asiatica Extract, Xylitol, Sodium Hyaluronate, Hydrolyzed Hyaluronic Acid, Hyaluronic Acid, Polyglyceryl-3 Methylglucose Distearate, Synthetic Beeswax, Hydrogenated Lecithin, Ammonium Acryloyldimethyltaurate/VP Copolymer, Silica, Vinyl Dimethicone, Hydroxyethyl Acrylate/Sodium Acryloyldimethyl Taurate Copolymer, Glyceryl Stearate, Caprylyl Glycol, Glucose, Fructooligosaccharides, Fructose, Acrylates/C10-30 Alkyl Acrylate Crosspolymer, Ethylhexylglycerin, Tromethamine, Sodium Phytate, Tocopherol, Madecassoside, Anhydroxylitol, Asiaticoside, Ceramide NP, Hydroxypropyltrimonium Hyaluronate, Sodium Acetylated Hyaluronate, Sodium Stearoyl Glutamate, Hydrolyzed Sodium Hyaluronate, Sodium Hyaluronate Crosspolymer, Potassium Hyaluronate, Cetyl Alcohol, Xanthan Gum, Limonene, Hydroxycitronellal, Citronellol, Butylphenyl Methylpropional",
+  },
+  {
+    idx: 4,
+    name: "SW19 9pm HAND CREAM (50ml)",
+    category: "Hand Care",
+    price: "23,000",
+    desc: `The SW19 perfume-scented hand cream is enriched with silky powder and mild botanical moisturizing ingredients to nourish and moisturize hands without sticky after-feel.
+
+    SW19 퍼퓸 향을 그대로 담은 핸드크림은 실키 파우더가 함유되어 끈적임 없이 매끄럽게 피부결을 정돈하며, 비건 포뮬러로 만들어진 순한 식물성 보습 성분이 피부를 산뜻하고 건강하게 유지합니다.
+
+    Ingredients certified by the EVE VEGAN® MARK`,
+    top: "Pink Pepper, Orange flower, Clove , Marshmallow accord Lotus",
+    heart: "Chestnut accord, Gaiac wood, Cade",
+    base: "Sandalwood, Vanilla Accord, Peru balsam, Cashmeran",
+    perfumer: "Florian Gallo",
+    ingredients: "Water, Glycerin, Butylene Glycol, Butyrospermum Parkii (Shea) Butter, Cetearyl Alcohol, Hydrogenated Polydecene, Fragrance(Parfum), 1,2-Hexanediol, Caprylic/Capric Triglyceride, Macadamia Integrifolia Seed Oil, Chlorella Vulgaris Extract, Melia Azadirachta Flower Extract, Ocimum Sanctum Leaf Extract, Melia Azadirachta Leaf Extract, Curcuma Longa (Turmeric) Root Extract, Corallina Officinalis Extract, Xylitylglucoside, Centella Asiatica Leaf Extract, Centella Asiatica Extract, Xylitol, Sodium Hyaluronate, Hydrolyzed Hyaluronic Acid, Hyaluronic Acid, Polyglyceryl-3 Methylglucose Distearate, Synthetic Beeswax, Hydrogenated Lecithin, Ammonium Acryloyldimethyltaurate/VP Copolymer, Silica, Vinyl Dimethicone, Hydroxyethyl Acrylate/Sodium Acryloyldimethyl Taurate Copolymer, Glyceryl Stearate, Caprylyl Glycol, Glucose, Fructooligosaccharides, Fructose, Acrylates/C10-30 Alkyl Acrylate Crosspolymer, Ethylhexylglycerin, Tromethamine, Sodium Phytate, Tocopherol, Madecassoside, Anhydroxylitol, Asiaticoside, Ceramide NP, Hydroxypropyltrimonium Hyaluronate, Sodium Acetylated Hyaluronate, Sodium Stearoyl Glutamate, Hydrolyzed Sodium Hyaluronate, Sodium Hyaluronate Crosspolymer, Potassium Hyaluronate, Cetyl Alcohol, Xanthan Gum, Eugenol, Cinnamal, Limonene, Linalool",
+  },
+  {
+    idx: 5,
+    name: "SW19 Midnight HAND CREAM (50ml)",
+    category: "Hand Care",
+    price: "23,000",
+    desc: `The SW19 perfume-scented hand cream is enriched with silky powder and mild botanical moisturizing ingredients to nourish and moisturize hands without sticky after-feel.
+
+    SW19 퍼퓸 향을 그대로 담은 핸드크림은 실키 파우더가 함유되어 끈적임 없이 매끄럽게 피부결을 정돈하며, 비건 포뮬러로 만들어진 순한 식물성 보습 성분이 피부를 산뜻하고 건강하게 유지합니다.
+
+    Ingredients certified by the EVE VEGAN® MARK`,
+    top: "Lily of The Valley, White Lotus",
+    heart: "White Musk, Powdery Notes",
+    base: "Sensual Musk, Sandalwood, Benzoin",
+    perfumer: "Marianne Nawrocki Sabatier",
+    ingredients: "Water, Glycerin, Butylene Glycol, Butyrospermum Parkii (Shea) Butter, Cetearyl Alcohol, Hydrogenated Polydecene, Fragrance(Parfum), 1,2-Hexanediol, Caprylic/Capric Triglyceride, Macadamia Integrifolia Seed Oil, Chlorella Vulgaris Extract, Melia Azadirachta Flower Extract, Ocimum Sanctum Leaf Extract, Melia Azadirachta Leaf Extract, Curcuma Longa (Turmeric) Root Extract, Corallina Officinalis Extract, Xylitylglucoside, Centella Asiatica Leaf Extract, Xylitol, Centella Asiatica Extract, Sodium Hyaluronate, Hydrolyzed Hyaluronic Acid, Hyaluronic Acid, Polyglyceryl-3 Methylglucose Distearate, Synthetic Beeswax, Hydrogenated Lecithin, Ammonium Acryloyldimethyltaurate/VP Copolymer, Silica, Vinyl Dimethicone, Hydroxyethyl Acrylate/Sodium Acryloyldimethyl Taurate Copolymer, Glyceryl Stearate, Caprylyl Glycol, Glucose, Fructooligosaccharides, Fructose, Ethylhexylglycerin, Acrylates/C10-30 Alkyl Acrylate Crosspolymer, Tromethamine, Sodium Phytate, Beta-Glucan, Tocopherol, Madecassoside, Anhydroxylitol, Asiaticoside, Ceramide NP, Hydroxypropyltrimonium Hyaluronate, Sodium Acetylated Hyaluronate, Sodium Stearoyl Glutamate, Hydrolyzed Sodium Hyaluronate, Sodium Hyaluronate Crosspolymer, Potassium Hyaluronate, Cetyl Alcohol, Xanthan Gum, Limonene, Linalool",
+  },
+  {
+    idx: 6,
+    name: "SW19 HAND CREAM TRIO SET",
+    category: "Hand Care",
+    price: "69,000",
+    desc: `The SW19 perfume-scented hand cream is enriched with silky powder and mild botanical moisturizing ingredients to nourish and moisturize hands without sticky after-feel.
+
+    SW19 퍼퓸 향을 그대로 담은 핸드크림은 실키 파우더가 함유되어 끈적임 없이 매끄럽게 피부결을 정돈하며, 비건 포뮬러로 만들어진 순한 식물성 보습 성분이 피부를 산뜻하고 건강하게 유지합니다.
+
+    Ingredients certified by the EVE VEGAN® MARK`,
+    top: "",
+    heart: "",
+    base: "",
+    perfumer: "",
+    ingredients: "",
+  },
+  {
+    idx: 2,
+    name: "SW19 HAND CREAM MINI SET (10ml*5)",
+    category: "Hand Care",
+    price: "30,000",
+    desc: ``,
+    top: "",
+    heart: "",
+    base: "",
+    perfumer: "",
+    ingredients: "",
+  },
+  {
+    idx: 2,
+    name: "SW19 6am HAND WASH (280ml)",
+    category: "Hand Care",
+    price: "34,000",
+    desc: ``,
+    top: "Bergamot, Grapefruit, Anise Seeds",
+    heart: "Cardamom, Clary Sage, Basil",
+    base: "Wormwood, Cedarwood, Musk",
+    perfumer: "Aliénor Massenet",
+    ingredients: "",
+  },
+  {
+    idx: 2,
+    name: "SW19 Noon HAND WASH (280ml)",
+    category: "Hand Care",
+    price: "34,000",
+    desc: ``,
+    top: "Green Leaf, Aqua, Mint, Almond Blossom",
+    heart: "Vetiver, Lavender, Lily of The Vally",
+    base: "Orris(Iris), Cedar Wood, Amber, Musk",
+    perfumer: "Marianne Nawrocki Sabatier",
+    ingredients: "",
+  },
+  {
+    idx: 2,
+    name: "SW19 3pm HAND WASH (280ml)",
+    category: "Hand Care",
+    price: "34,000",
+    desc: ``,
+    top: "Bergamot, Rhubarb, Tangerine Lotus",
+    heart: "Cedarwood, Jasmine, Gardenia",
+    base: "White Musk, Vetiver, Orris",
+    perfumer: "Nathalie Benareau",
+    ingredients: "",
+  },
+  {
+    idx: 2,
+    name: "SW19 9pm HAND WASH (280ml)",
+    category: "Hand Care",
+    price: "34,000",
+    desc: ``,
+    top: "Pink Pepper, Orange flower, Clove , Marshmallow accord Lotus",
+    heart: "Chestnut accord, Gaiac wood, Cade",
+    base: "Sandalwood, Vanilla Accord, Peru balsam, Cashmeran",
+    perfumer: "Florian Gallo",
+    ingredients: "",
+  },
+  {
+    idx: 2,
+    name: "SW19 Midnight HAND WASH (280ml)",
+    category: "Hand Care",
+    price: "34,000",
+    desc: ``,
+    top: "Lily of The Valley, White Lotus",
+    heart: "White Musk, Powdery Notes",
+    base: "Sensual Musk, Sandalwood, Benzoin",
+    perfumer: "Marianne Nawrocki Sabatier",
+    ingredients: "",
+  },
+  {
+    idx: 2,
+    name: "SW19 6am HAND CARE SET",
+    category: "Hand Care",
+    price: "57,00057,000",
+    desc: ``,
+    top: "Bergamot, Grapefruit, Anise Seeds",
+    heart: "Cardamom, Clary Sage, Basil",
+    base: "Wormwood, Cedarwood, Musk",
+    perfumer: "Aliénor Massenet",
+    ingredients: "",
+  },
+  {
+    idx: 2,
+    name: "SW19 Noon HAND CARE SET",
+    category: "Hand Care",
+    price: "57,000",
+    desc: ``,
+    top: "Green Leaf, Aqua, Mint, Almond Blossom",
+    heart: "Vetiver, Lavender, Lily of The Vally",
+    base: "Orris(Iris), Cedar Wood, Amber, Musk",
+    perfumer: "Marianne Nawrocki Sabatier",
+    ingredients: "",
+  },
+  {
+    idx: 2,
+    name: "SW19 3pm HAND CARE SET",
+    category: "Hand Care",
+    price: "57,000",
+    desc: ``,
+    top: "Bergamot, Rhubarb, Tangerine Lotus",
+    heart: "Cedarwood, Jasmine, Gardenia",
+    base: "White Musk, Vetiver, Orris",
+    perfumer: "Nathalie Benareau",
+    ingredients: "",
+  },
+  {
+    idx: 2,
+    name: "SW19 9pm HAND CARE SET",
+    category: "Hand Care",
+    price: "57,000",
+    desc: ``,
+    top: "Pink Pepper, Orange flower, Clove , Marshmallow accord Lotus",
+    heart: "Chestnut accord, Gaiac wood, Cade",
+    base: "Sandalwood, Vanilla Accord, Peru balsam, Cashmeran",
+    perfumer: "Florian Gallo",
+    ingredients: "",
+  },
+  {
+    idx: 2,
+    name: "SW19 Midnight HAND CARE SET",
+    category: "Hand Care",
+    price: "57,000",
+    desc: ``,
+    top: "Lily of The Valley, White Lotus",
+    heart: "White Musk, Powdery Notes",
+    base: "Sensual Musk, Sandalwood, Benzoin",
+    perfumer: "Marianne Nawrocki Sabatier",
+    ingredients: "",
+  },
+];
