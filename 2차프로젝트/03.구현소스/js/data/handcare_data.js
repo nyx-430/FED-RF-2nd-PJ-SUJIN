8eb9fb238d919067747c1069fc6c66d0f39d33e3
Sujin <9_ight@naver.com>
2024-06-11 15:53:52 +0900

    Update handcare_data.js

--- 2차프로젝트/03.구현소스/js/data/handcare_data.js
+++ 2차프로젝트/03.구현소스/js/data/handcare_data.js
@@ -102,11 +102,15 @@ export const handData = [
     ingredients: "",
   },
   {
-    idx: 2,
+    idx: 7,
     name: "SW19 HAND CREAM MINI SET (10ml*5)",
     category: "Hand Care",
     price: "30,000",
-    desc: ``,
+    desc: `The SW19 perfume-scented hand cream is enriched with silky powder and mild botanical moisturizing ingredients to nourish and moisturize hands without sticky after-feel.
+
+    SW19 퍼퓸 향을 그대로 담은 핸드크림은 실키 파우더가 함유되어 끈적임 없이 매끄럽게 피부결을 정돈하며, 비건 포뮬러로 만들어진 순한 식물성 보습 성분이 피부를 산뜻하고 건강하게 유지합니다.
+
+    Ingredients certified by the EVE VEGAN® MARK`,
     top: "",
     heart: "",
     base: "",
@@ -114,83 +118,119 @@ export const handData = [
     ingredients: "",
   },
   {
-    idx: 2,
+    idx: 8,
     name: "SW19 6am HAND WASH (280ml)",
     category: "Hand Care",
     price: "34,000",
-    desc: ``,
+    desc: `SW19 hand wash, made of plant-based formula, provides a long-lasting scent of SW19 perfume. Using botanical and coconut extracts, it leaves the hands smooth and moisturized after use.
+
+    비건 포뮬러로 만들어진 SW19 핸드워시는 퍼퓸 본연의 향을 그대로 담아 세정 후에도 은은한 향기가 지속됩니다. 식물 유래 성분과 코코넛 유래 성분을 함유하여 사용 후에도 손을 촉촉하게 유지합니다.
+
+    Ingredients certified by the EVE VEGAN® MARK`,
     top: "Bergamot, Grapefruit, Anise Seeds",
     heart: "Cardamom, Clary Sage, Basil",
     base: "Wormwood, Cedarwood, Musk",
-    perfumer: "Aliénor Massenet",
-    ingredients: "",
+    perfumer: "Aliénor Massenet - VP Senior Perfumer at Symrise",
+    ingredients: "Water, Decyl Glucoside, Disodium Cocoamphodiacetate, Sodium Chloride, Sodium C14-16 Olefin Sulfonate, Octyldodeceth-16, PEG-60 Hydrogenated Castor Oil, Fragrance(Parfum), Alcohol Denat, Chlorella Vulgaris Extract, Glycine Soja (Soybean) Seed Extract, Quillaja Saponaria Bark Extract, Xylitylglucoside, Centella Asiatica Leaf Extract, Xylitol, Centella Asiatica Extract, Sodium Hyaluronate, Hydrolyzed Hyaluronic Acid, Hyaluronic Acid, 1,2-Hexanediol, Coco-Glucoside, Citric Acid, Glyceryl Caprylate, Glucose, Butylene Glycol, Fructooligosaccharides, Fructose, Sodium Cocoyl Isethionate, Hexylene Glycol, Sodium Phytate, Glycerin, Pentylene Glycol, Octanediol, Ethylhexylglycerin, Cocamide Methyl MEA, Madecassoside, Anhydroxylitol, Asiaticoside, Caprylic/Capric Triglyceride, Hydrogenated Lecithin, Ceramide NP, Hydroxypropyltrimonium Hyaluronate, Sodium Acetylated Hyaluronate, Sodium Stearoyl Glutamate, Hydrolyzed Sodium Hyaluronate, Sodium Hyaluronate Crosspolymer, Potassium Hyaluronate, Linalool, Limonene",
   },
   {
-    idx: 2,
+    idx: 9,
     name: "SW19 Noon HAND WASH (280ml)",
     category: "Hand Care",
     price: "34,000",
-    desc: ``,
+    desc: `SW19 hand wash, made of plant-based formula, provides a long-lasting scent of SW19 perfume. Using botanical and coconut extracts, it leaves the hands smooth and moisturized after use.
+
+    비건 포뮬러로 만들어진 SW19 핸드워시는 퍼퓸 본연의 향을 그대로 담아 세정 후에도 은은한 향기가 지속됩니다. 식물 유래 성분과 코코넛 유래 성분을 함유하여 사용 후에도 손을 촉촉하게 유지합니다.
+
+    Ingredients certified by the EVE VEGAN® MARK`,
     top: "Green Leaf, Aqua, Mint, Almond Blossom",
     heart: "Vetiver, Lavender, Lily of The Vally",
     base: "Orris(Iris), Cedar Wood, Amber, Musk",
     perfumer: "Marianne Nawrocki Sabatier",
-    ingredients: "",
+    ingredients: "Water, Decyl Glucoside, Disodium Cocoamphodiacetate, Sodium Chloride, Sodium C14-16 Olefin Sulfonate, Octyldodeceth-16, PEG-60 Hydrogenated Castor Oil, Fragrance(Parfum), Alcohol Denat, Chlorella Vulgaris Extract, Glycine Soja (Soybean) Seed Extract, Quillaja Saponaria Bark Extract, Xylitylglucoside, Centella Asiatica Leaf Extract, Xylitol, Centella Asiatica Extract, Sodium Hyaluronate, Hydrolyzed Hyaluronic Acid, Hyaluronic Acid, 1,2-Hexanediol, Coco-Glucoside, Citric Acid, Glyceryl Caprylate, Glucose, Butylene Glycol, Fructooligosaccharides, Fructose, Sodium Cocoyl Isethionate, Hexylene Glycol, Sodium Phytate, Glycerin, Pentylene Glycol, Octanediol, Ethylhexylglycerin, Cocamide Methyl MEA, Madecassoside, Anhydroxylitol, Asiaticoside, Caprylic/Capric Triglyceride, Hydrogenated Lecithin, Ceramide NP, Hydroxypropyltrimonium Hyaluronate, Sodium Acetylated Hyaluronate, Sodium Stearoyl Glutamate, Hydrolyzed Sodium Hyaluronate, Sodium Hyaluronate Crosspolymer, Potassium Hyaluronate, Eugenol",
   },
   {
-    idx: 2,
+    idx: 10,
     name: "SW19 3pm HAND WASH (280ml)",
     category: "Hand Care",
     price: "34,000",
-    desc: ``,
+    desc: `SW19 hand wash, made of plant-based formula, provides a long-lasting scent of SW19 perfume. Using botanical and coconut extracts, it leaves the hands smooth and moisturized after use.
+
+    비건 포뮬러로 만들어진 SW19 핸드워시는 퍼퓸 본연의 향을 그대로 담아 세정 후에도 은은한 향기가 지속됩니다. 식물 유래 성분과 코코넛 유래 성분을 함유하여 사용 후에도 손을 촉촉하게 유지합니다.
+
+    Ingredients certified by the EVE VEGAN® MARK`,
     top: "Bergamot, Rhubarb, Tangerine Lotus",
     heart: "Cedarwood, Jasmine, Gardenia",
     base: "White Musk, Vetiver, Orris",
-    perfumer: "Nathalie Benareau",
-    ingredients: "",
+    perfumer: "Nathalie Benareau - Master Perfumer at Symrise",
+    ingredients: "Water, Decyl Glucoside, Disodium Cocoamphodiacetate, Sodium Chloride, Sodium C14-16 Olefin Sulfonate, Octyldodeceth-16, PEG-60 Hydrogenated Castor Oil, Fragrance(Parfum), Alcohol Denat, Chlorella Vulgaris Extract, Glycine Soja (Soybean) Seed Extract, Quillaja Saponaria Bark Extract, Xylitylglucoside, Centella Asiatica Leaf Extract, Xylitol, Centella Asiatica Extract, Sodium Hyaluronate, Hydrolyzed Hyaluronic Acid, Hyaluronic Acid, 1,2-Hexanediol, Coco-Glucoside, Citric Acid, Glyceryl Caprylate, Glucose, Butylene Glycol, Fructooligosaccharides, Fructose, Sodium Cocoyl Isethionate, Hexylene Glycol, Sodium Phytate, Glycerin, Pentylene Glycol, Octanediol, Ethylhexylglycerin, Cocamide Methyl MEA, Madecassoside, Anhydroxylitol, Asiaticoside, Caprylic/Capric Triglyceride, Hydrogenated Lecithin, Ceramide NP, Hydroxypropyltrimonium Hyaluronate, Sodium Acetylated Hyaluronate, Sodium Stearoyl Glutamate, Hydrolyzed Sodium Hyaluronate, Sodium Hyaluronate Crosspolymer, Potassium Hyaluronate, Limonene, Hydroxycitronellal, Citronellol, Butylphenyl Methylpropional",
   },
   {
-    idx: 2,
+    idx: 11,
     name: "SW19 9pm HAND WASH (280ml)",
     category: "Hand Care",
     price: "34,000",
-    desc: ``,
+    desc: `SW19 hand wash, made of plant-based formula, provides a long-lasting scent of SW19 perfume. Using botanical and coconut extracts, it leaves the hands smooth and moisturized after use.
+
+    비건 포뮬러로 만들어진 SW19 핸드워시는 퍼퓸 본연의 향을 그대로 담아 세정 후에도 은은한 향기가 지속됩니다. 식물 유래 성분과 코코넛 유래 성분을 함유하여 사용 후에도 손을 촉촉하게 유지합니다.
+
+    Ingredients certified by the EVE VEGAN® MARK`,
     top: "Pink Pepper, Orange flower, Clove , Marshmallow accord Lotus",
     heart: "Chestnut accord, Gaiac wood, Cade",
     base: "Sandalwood, Vanilla Accord, Peru balsam, Cashmeran",
     perfumer: "Florian Gallo",
-    ingredients: "",
+    ingredients: "Water, Decyl Glucoside, Disodium Cocoamphodiacetate, Sodium Chloride, Sodium C14-16 Olefin Sulfonate, Octyldodeceth-16, PEG-60 Hydrogenated Castor Oil, Fragrance(Parfum), Alcohol Denat, Chlorella Vulgaris Extract, Glycine Soja (Soybean) Seed Extract, Quillaja Saponaria Bark Extract, Xylitylglucoside, Centella Asiatica Leaf Extract, Xylitol, Centella Asiatica Extract, Sodium Hyaluronate, Hydrolyzed Hyaluronic Acid, Hyaluronic Acid, 1,2-Hexanediol, Coco-Glucoside, Citric Acid, Glyceryl Caprylate, Glucose, Butylene Glycol, Fructooligosaccharides, Fructose, Sodium Cocoyl Isethionate, Hexylene Glycol, Sodium Phytate, Glycerin, Pentylene Glycol, Octanediol, Ethylhexylglycerin, Cocamide Methyl MEA, Madecassoside, Anhydroxylitol, Asiaticoside, Caprylic/Capric Triglyceride, Hydrogenated Lecithin, Ceramide NP, Hydroxypropyltrimonium Hyaluronate, Sodium Acetylated Hyaluronate, Sodium Stearoyl Glutamate, Hydrolyzed Sodium Hyaluronate, Sodium Hyaluronate Crosspolymer, Potassium Hyaluronate, Eugenol",
   },
   {
-    idx: 2,
+    idx: 12,
     name: "SW19 Midnight HAND WASH (280ml)",
     category: "Hand Care",
     price: "34,000",
-    desc: ``,
+    desc: `SW19 hand wash, made of plant-based formula, provides a long-lasting scent of SW19 perfume. Using botanical and coconut extracts, it leaves the hands smooth and moisturized after use.
+
+    비건 포뮬러로 만들어진 SW19 핸드워시는 퍼퓸 본연의 향을 그대로 담아 세정 후에도 은은한 향기가 지속됩니다. 식물 유래 성분과 코코넛 유래 성분을 함유하여 사용 후에도 손을 촉촉하게 유지합니다.
+
+    Ingredients certified by the EVE VEGAN® MARK`,
     top: "Lily of The Valley, White Lotus",
     heart: "White Musk, Powdery Notes",
     base: "Sensual Musk, Sandalwood, Benzoin",
     perfumer: "Marianne Nawrocki Sabatier",
-    ingredients: "",
+    ingredients: "Water, Decyl Glucoside, Disodium Cocoamphodiacetate, Sodium Chloride, Sodium C14-16 Olefin Sulfonate, Octyldodeceth-16, PEG-60 Hydrogenated Castor Oil, Fragrance(Parfum), Alcohol Denat, Chlorella Vulgaris Extract, Glycine Soja (Soybean) Seed Extract, Quillaja Saponaria Bark Extract, Xylitylglucoside, Centella Asiatica Leaf Extract, Xylitol, Centella Asiatica Extract, Sodium Hyaluronate, Hydrolyzed Hyaluronic Acid, Hyaluronic Acid, 1,2-Hexanediol, Coco-Glucoside, Citric Acid, Glyceryl Caprylate, Glucose, Butylene Glycol, Fructooligosaccharides, Fructose, Sodium Cocoyl Isethionate, Hexylene Glycol, Sodium Phytate, Glycerin, Pentylene Glycol, Octanediol, Ethylhexylglycerin, Cocamide Methyl MEA, Madecassoside, Anhydroxylitol, Asiaticoside, Caprylic/Capric Triglyceride, Hydrogenated Lecithin, Ceramide NP, Hydroxypropyltrimonium Hyaluronate, Sodium Acetylated Hyaluronate, Sodium Stearoyl Glutamate, Hydrolyzed Sodium Hyaluronate, Sodium Hyaluronate Crosspolymer, Potassium Hyaluronate, Eugenol",
   },
   {
-    idx: 2,
+    idx: 13,
     name: "SW19 6am HAND CARE SET",
     category: "Hand Care",
     price: "57,00057,000",
-    desc: ``,
+    desc: `Introducing SW19 Hand Care Set, which carries the perfume's authentic scent and leaves a subtle scent.Take care of your skin with a hand wash that keeps your hand hydrated even after shower and a hand cream that that leaves your skin feeling smooth without sensation of tackiness.
+
+    Both products are vegan formulas and have obtained the French Yves vegan certification mark.
+
+    퍼퓸 본연의 향을 담아 사용 후 은은한 향기가 지속되는 SW19 핸드케어 세트를 만나보세요. 세정 후에도 손을 촉촉하게 유지하는 핸드워시와 끈적임 없이 매끄럽게 피부결을 정돈하는 핸드크림으로 손 피부를 관리하세요.
+
+    두 제품 모두 비건 포뮬러로 프랑스 이브사의 비건 인증 마크를 획득한 제품입니다.
+
+    Ingredients certified by the EVE VEGAN® MARK`,
     top: "Bergamot, Grapefruit, Anise Seeds",
     heart: "Cardamom, Clary Sage, Basil",
     base: "Wormwood, Cedarwood, Musk",
-    perfumer: "Aliénor Massenet",
+    perfumer: "Aliénor Massenet - VP Senior Perfumer at Symrise",
     ingredients: "",
   },
   {
-    idx: 2,
+    idx: 14,
     name: "SW19 Noon HAND CARE SET",
     category: "Hand Care",
     price: "57,000",
-    desc: ``,
+    desc: `Introducing SW19 Hand Care Set, which carries the perfume's authentic scent and leaves a subtle scent.Take care of your skin with a hand wash that keeps your hand hydrated even after shower and a hand cream that that leaves your skin feeling smooth without sensation of tackiness.
+
+    Both products are vegan formulas and have obtained the French Yves vegan certification mark.
+
+    퍼퓸 본연의 향을 담아 사용 후 은은한 향기가 지속되는 SW19 핸드케어 세트를 만나보세요. 세정 후에도 손을 촉촉하게 유지하는 핸드워시와 끈적임 없이 매끄럽게 피부결을 정돈하는 핸드크림으로 손 피부를 관리하세요.
+
+    두 제품 모두 비건 포뮬러로 프랑스 이브사의 비건 인증 마크를 획득한 제품입니다.
+
+    Ingredients certified by the EVE VEGAN® MARK`,
     top: "Green Leaf, Aqua, Mint, Almond Blossom",
     heart: "Vetiver, Lavender, Lily of The Vally",
     base: "Orris(Iris), Cedar Wood, Amber, Musk",
@@ -198,23 +238,39 @@ export const handData = [
     ingredients: "",
   },
   {
-    idx: 2,
+    idx: 15,
     name: "SW19 3pm HAND CARE SET",
     category: "Hand Care",
     price: "57,000",
-    desc: ``,
+    desc: `Introducing SW19 Hand Care Set, which carries the perfume's authentic scent and leaves a subtle scent.Take care of your skin with a hand wash that keeps your hand hydrated even after shower and a hand cream that that leaves your skin feeling smooth without sensation of tackiness.
+
+    Both products are vegan formulas and have obtained the French Yves vegan certification mark.
+
+    퍼퓸 본연의 향을 담아 사용 후 은은한 향기가 지속되는 SW19 핸드케어 세트를 만나보세요. 세정 후에도 손을 촉촉하게 유지하는 핸드워시와 끈적임 없이 매끄럽게 피부결을 정돈하는 핸드크림으로 손 피부를 관리하세요.
+
+    두 제품 모두 비건 포뮬러로 프랑스 이브사의 비건 인증 마크를 획득한 제품입니다.
+
+    Ingredients certified by the EVE VEGAN® MARK`,
     top: "Bergamot, Rhubarb, Tangerine Lotus",
     heart: "Cedarwood, Jasmine, Gardenia",
     base: "White Musk, Vetiver, Orris",
-    perfumer: "Nathalie Benareau",
+    perfumer: "Nathalie Benareau - Master Perfumer at Symrise",
     ingredients: "",
   },
   {
-    idx: 2,
+    idx: 16,
     name: "SW19 9pm HAND CARE SET",
     category: "Hand Care",
     price: "57,000",
-    desc: ``,
+    desc: `Introducing SW19 Hand Care Set, which carries the perfume's authentic scent and leaves a subtle scent.Take care of your skin with a hand wash that keeps your hand hydrated even after shower and a hand cream that that leaves your skin feeling smooth without sensation of tackiness.
+
+    Both products are vegan formulas and have obtained the French Yves vegan certification mark.
+
+    퍼퓸 본연의 향을 담아 사용 후 은은한 향기가 지속되는 SW19 핸드케어 세트를 만나보세요. 세정 후에도 손을 촉촉하게 유지하는 핸드워시와 끈적임 없이 매끄럽게 피부결을 정돈하는 핸드크림으로 손 피부를 관리하세요.
+
+    두 제품 모두 비건 포뮬러로 프랑스 이브사의 비건 인증 마크를 획득한 제품입니다.
+
+    Ingredients certified by the EVE VEGAN® MARK`,
     top: "Pink Pepper, Orange flower, Clove , Marshmallow accord Lotus",
     heart: "Chestnut accord, Gaiac wood, Cade",
     base: "Sandalwood, Vanilla Accord, Peru balsam, Cashmeran",
@@ -222,11 +278,19 @@ export const handData = [
     ingredients: "",
   },
   {
-    idx: 2,
+    idx: 17,
     name: "SW19 Midnight HAND CARE SET",
     category: "Hand Care",
     price: "57,000",
-    desc: ``,
+    desc: `Introducing SW19 Hand Care Set, which carries the perfume's authentic scent and leaves a subtle scent.Take care of your skin with a hand wash that keeps your hand hydrated even after shower and a hand cream that that leaves your skin feeling smooth without sensation of tackiness.
+
+    Both products are vegan formulas and have obtained the French Yves vegan certification mark.
+
+    퍼퓸 본연의 향을 담아 사용 후 은은한 향기가 지속되는 SW19 핸드케어 세트를 만나보세요. 세정 후에도 손을 촉촉하게 유지하는 핸드워시와 끈적임 없이 매끄럽게 피부결을 정돈하는 핸드크림으로 손 피부를 관리하세요.
+
+    두 제품 모두 비건 포뮬러로 프랑스 이브사의 비건 인증 마크를 획득한 제품입니다.
+
+    Ingredients certified by the EVE VEGAN® MARK`,
     top: "Lily of The Valley, White Lotus",
     heart: "White Musk, Powdery Notes",
     base: "Sensual Musk, Sandalwood, Benzoin",
