b38bc6f08c2d1add11bcbc624e2c47d7851b7e1c
Sujin <9_ight@naver.com>
2024-06-17 17:56:10 +0900

    17:56

--- 2차프로젝트/03.구현소스/sw-app/src/components/pages/Story.jsx
+++ 2차프로젝트/03.구현소스/sw-app/src/components/pages/Story.jsx
@@ -1,9 +1,15 @@
+// 스토리 페이지 컴포넌트
+
 import React from 'react';
+
+// 컴포넌트 불러오기
+import StoryIntro from '../modules/StoryIntro';
 
 function Story(props) {
     return (
         <>
-            <h1>Story 페이지</h1>
+            {/* <!-- 스토리 인트로 컴포넌트 --> */}
+            <StoryIntro />
         </>
     );
 }
