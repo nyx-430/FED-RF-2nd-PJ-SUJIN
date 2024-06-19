94a42610261767c61aae69864c1c7dab34175534
Sujin <9_ight@naver.com>
2024-06-19 16:46:59 +0900

    Update gnb.js

--- 2차프로젝트/03.구현소스/sw-app/src/components/data/gnb.js
+++ 2차프로젝트/03.구현소스/sw-app/src/components/data/gnb.js
@@ -1,54 +1,62 @@
 // SW19 GNB 메뉴 데이터
 
 export const menu = [
-    {
-        txt: "HOME",
-        link: "/",
-    },
-    {
-        txt: "SHOP",
-        link: "/shop",
-        sub: [
-            {
-                txt: "All Products",
-                link: "/products",
-            },
-            {
-                txt: "Promotion",
-                link: "/promotion",
-            },
-            {
-                txt: "Perfume",
-                link: "/perfume",
-            },
-            {
-                txt: "Body Care",
-                link: "/bodycare",
-            },
-            {
-                txt: "Hand Care",
-                link: "/handcare",
-            },
-            {
-                txt: "Home Fragrance",
-                link: "/fragrance",
-            },
-            {
-                txt: "Gift Set",
-                link: "/set",
-            },
-            {
-                txt: "Tray",
-                link: "/tray",
-            },
-            {
-                txt: "Accessory",
-                link: "/accessory",
-            },
-            {
-                txt: "Sample Kit",
-                link: "/kit",
-            },
-        ]
-    },
-]
+  {
+    txt: "HOME",
+    link: "/",
+  },
+  {
+    txt: "SHOP",
+    link: "/shop",
+    sub: [
+      {
+        txt: "All Products",
+        link: "/products",
+      },
+      {
+        txt: "Promotion",
+        link: "/promotion",
+      },
+      {
+        txt: "Perfume",
+        link: "/perfume",
+      },
+      {
+        txt: "Body Care",
+        link: "/bodycare",
+      },
+      {
+        txt: "Hand Care",
+        link: "/handcare",
+      },
+      {
+        txt: "Home Fragrance",
+        link: "/fragrance",
+      },
+      {
+        txt: "Gift Set",
+        link: "/set",
+      },
+      {
+        txt: "Tray",
+        link: "/tray",
+      },
+      {
+        txt: "Accessory",
+        link: "/accessory",
+      },
+      {
+        txt: "Sample Kit",
+        link: "/kit",
+      },
+    ],
+  },
+  {
+    txt: "STORY",
+    link: "/story",
+  },
+  {
+    txt: "MEMBERS SHIP",
+    link: "/membership",
+  },
+];
