592c40409d6e5523cf11b0ff020eb44a801329d8
Sujin <9_ight@naver.com>
2024-06-19 16:20:19 +0900

    16:20

---
+++ 2차프로젝트/03.구현소스/sw-app/src/components/data/gnb.js
@@ -0,0 +1,50 @@
+// SW19 GNB 메뉴 데이터
+
+export const menu = [
+    {
+        txt: "HOME",
+        link: "/",
+    },
+    {
+        txt: "SHOP",
+        link: "/shop",
+        sub: [
+            {
+                txt: "All Products",
+                link: "Promotion",
+            },
+            {
+                txt: "",
+                link: "",
+            },
+            {
+                txt: "",
+                link: "",
+            },
+            {
+                txt: "",
+                link: "",
+            },
+            {
+                txt: "",
+                link: "",
+            },
+            {
+                txt: "",
+                link: "",
+            },
+            {
+                txt: "",
+                link: "",
+            },
+            {
+                txt: "",
+                link: "",
+            },
+            {
+                txt: "",
+                link: "",
+            },
+        ]
+    },
+]
---
+++ 2차프로젝트/03.구현소스/sw-app/src/components/modules/Logo.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Logo(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Logo;
