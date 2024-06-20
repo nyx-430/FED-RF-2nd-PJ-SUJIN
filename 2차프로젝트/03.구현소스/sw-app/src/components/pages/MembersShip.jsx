d3416f81fdabbfaf10d931f090d757c22e36c01d
Sujin <9_ight@naver.com>
2024-06-20 18:07:23 +0900

    18:07

---
+++ 2차프로젝트/03.구현소스/sw-app/src/components/pages/MembersShip.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MembersShip(props) {
+    return (
+        <div>
+            <h1>멤버십 페이지</h1>
+        </div>
+    );
+}
+
+export default MembersShip;
---
+++ 2차프로젝트/03.구현소스/sw-app/src/components/pages/Shop.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Shop(props) {
+    return (
+        <div>
+            <h1>판매 페이지</h1>
+        </div>
+    );
+}
+
+export default Shop;
