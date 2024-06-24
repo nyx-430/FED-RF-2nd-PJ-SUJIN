3f3b02237a65424e1bb108206abe9872480a5c34
Sujin <9_ight@naver.com>
2024-06-24 15:27:22 +0900

    15:27

--- 2차프로젝트/03.구현소스/sw-app/src/components/data/banner_data.js
+++ 2차프로젝트/03.구현소스/sw-app/src/components/data/banner_data.js
@@ -4,3 +4,7 @@ export const pmBan = {
         img: ["./images/promotion/MAIN_SLIDE_PC_SUMMER.jpg", "./images/promotion/MAIN_SLIDE_pc_bathduck_LOW.jpg"],
         tit: "The Summer, We Loved at 3PM",
 }
+
+const collBan = {
+        
+}
