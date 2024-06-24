3e16bd1a85991779bdda25a128a21ce89c4971c9
Sujin <9_ight@naver.com>
2024-06-24 12:03:37 +0900

    Update Promotion.jsx

--- 2차프로젝트/03.구현소스/sw-app/src/components/modules/Promotion.jsx
+++ 2차프로젝트/03.구현소스/sw-app/src/components/modules/Promotion.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { pmData } from "../data/pmData";
 
 function Promotion(props) {
   return (
@@ -6,30 +7,28 @@ function Promotion(props) {
       <div className="inbox">
         <h2 className="title">Promotion : The Summer, We Loved at 3PM</h2>
         <div className="banner-box">
-            <img src="./images/promotion/MAIN_SLIDE_PC_SUMMER.jpg" alt="promotion" />
+          <img
+            src="./images/promotion/MAIN_SLIDE_PC_SUMMER.jpg"
+            alt="promotion"
+          />
         </div>
         <div className="pbox ">
-        <h1 className="title">Product</h1>
+          <h1 className="title">Product</h1>
           <div className="col-12">
-              <ul>
-                <li>
+            <ul>
+              {pmData.map((v, i) => (
+                <li key={i}>
+                  <img src="./images/promotion/${v.img}" alt={v.tit} />
+                  <h2>{v.tit}</h2>
+                  <h3>{v.price}</h3>
+                </li>
+              ))}
+              {/* <li>
                   <img src="./images/set_6am.png" alt="6am" />
                   <h2>상품명</h2>
                   <h3>가격</h3>
-                </li>
-                <li>
-                  <img src="./images/set_noon.jpg" alt="noon" />
-                </li>
-                <li>
-                  <img src="./images/set_3pm.png" alt="3pm" />
-                </li>
-                <li>
-                  <img src="./images/set_9pm.png" alt="9pm" />
-                </li>
-                <li>
-                  <img src="./images/set_mid.jpg" alt="mid" />
-                </li>
-              </ul>
+                </li> */}
+            </ul>
           </div>
         </div>
       </div>
