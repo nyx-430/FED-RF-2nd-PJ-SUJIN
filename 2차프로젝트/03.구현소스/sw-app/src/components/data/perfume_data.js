9a46d273732600e924381b746116872857358c91
Sujin <9_ight@naver.com>
2024-06-26 16:07:28 +0900

    Update perfume_data.js

--- 2차프로젝트/03.구현소스/sw-app/src/components/data/perfume_data.js
+++ 2차프로젝트/03.구현소스/sw-app/src/components/data/perfume_data.js
@@ -8,7 +8,8 @@ export const perfumeData = [
     category: "Perfume",
     price: "109,000원",
     src: "./images/shop/6am/p_6am.png",
-    desc: `The scent of enchanted grass created by spellbound forest spirits&quot;
+    desc: `The scent of enchanted grass created by spellbound forest spirits
+        ^
         Just before the sunrise, the mist of Wimbledon forest drifts along and greets you as you take a stroll. Feel the 6AM scent of the Wimbledon forest’s glistening morning dew and moist soil.
         ^
         숲의 정령이 만들어낸 중독적인 묘약, 신비로운 풀의 향취
@@ -30,14 +31,18 @@ export const perfumeData = [
     category: "Perfume",
     price: "109,000원",
     src: "./images/shop/noon/p_noon.png",
-    desc: `&quot;The exquisite breeze around the lakeside bench filled with the harmonious scent of floral and aqua&quot;
+    desc: `The exquisite breeze around the lakeside bench filled with the harmonious scent of floral and aqua
+        ^
         Midday, when the sunlight sparkles on the surface of the lake, the bouquet of a full bloom iris and cool aqua scent under the tree shade fills up the air. Feel the scent of NOON at Wimbledon Park.
-
-        &quot;공원 호숫가 벤치에 불어오는 물과 꽃 향의 절묘한 조화&quot;
+        ^
+        공원 호숫가 벤치에 불어오는 물과 꽃 향의 절묘한 조화
+        ^
         호수에 반짝이는 햇살 찬란한 낮 12시, 서늘한 나무그늘 아래 차가운 물 향과 햇살에 만개한 아이리스 꽃 향이 어우러진 윔블던 공원의 정오를 느껴보세요.`,
     note: `
         -top: Green Leaf, Aqua, Mint, Almond Blossom
+        ^
         -heart: Vetiver, Lavender, Lily of The Vally
+        ^
         -base: Orris(Iris), Cedar Wood, Amber, Musk
         `,
     perfumer: "Marianne Nawrocki Sabatier",
@@ -50,14 +55,18 @@ export const perfumeData = [
     category: "Perfume",
     price: "109,000원",
     src: "./images/shop/3pm/p_3pm.png",
-    desc: `&quot;The sweet scent of ripened fruits combined with lustrous grass under the beaming sunlight&quot;
+    desc: `The sweet scent of ripened fruits combined with lustrous grass under the beaming sunlight
+        ^
         While the forest’s morning dew dries away in the beaming afternoon sunlight, feel the scent of the fully blossomed flowers in the park come together with the freshness of lemon floating on a cup of black tea at 3PM in Wimbledon.
-        
-        &quot;햇살이 내리쬐는 푸른 잔디와 부드럽고 향긋한 과일의 향취&quot;
+        ^
+        햇살이 내리쬐는 푸른 잔디와 부드럽고 향긋한 과일의 향취
+        ^
         이슬 젖은 초록숲이 상큼한 햇살에 말라가는 윔블던의 오후. 공원에 만개한 꽃들의 향기와 홍차에 띄운 레몬 조각 향기가 어우러진 윔블던의 오후 3시를 느껴보세요.`,
     note: `
     -top: Bergamot, Rhubarb, Tangerine Lotus
+    ^
     -heart: Cedarwood, Jasmine, Gardenia
+    ^
     -base: White Musk, Vetiver, Orris
     `,
     perfumer: "Nathalie Benareau",
@@ -70,14 +79,18 @@ export const perfumeData = [
     category: "Perfume",
     price: "109,000원",
     src: "./images/shop/9pm/p_9pm.png",
-    desc: `&quot;The romance of a silent night in the woods&quot;
+    desc: `The romance of a silent night in the woods
+        ^
         The night in Wimbledon forest after dark. The whispers of love travel through the crackling bonfire. Feel the Wimbledon’s 9PM scent of the smokey bonfire smoothly blended with sweet vanilla.
-        
-        &quot;고요한 숲 속의 밤을 채운 사랑의 설레임&quot;
+        ^
+        고요한 숲 속의 밤을 채운 사랑의 설레임
+        ^
         어둠이 내린 윔블던 숲의 저녁. 모닥불 타는 소리 사이로 들려오는 연인들의 달콤한 속삭임. 바닐라의 달콤함과 숲 속 모닥불의 스모키함이 어우러진 윔블던의 저녁 9시를 느껴보세요.`,
     note: `
     -top: Pink Pepper, Orange flower, Clove , Marshmallow accord Lotus
+    ^
     -heart: Chestnut accord, Gaiac wood, Cade
+    ^
     -base: Sandalwood, Vanilla Accord, Peru balsam, Cashmeran
     `,
     perfumer: "Florian Gallo",
@@ -90,14 +103,18 @@ export const perfumeData = [
     category: "Perfume",
     price: "109,000원",
     src: "./images/shop/midnight/p_mid.png",
-    desc: `&quot;Deep in the soundless night, the scent of two lovers unite to create one&quot;
+    desc: `Deep in the soundless night, the scent of two lovers unite to create one
+        ^
         In the still of the night, the soft moonlight surrounds the Wimbledon forest. Feel the sensual MIDNIGHT through mesmerizing touch and scent.
-
-        "&quot;모두가 잠든 깊은 밤, 서로의 향에 물들어 하나가 된 둘&quot;
+        ^
+        모두가 잠든 깊은 밤, 서로의 향에 물들어 하나가 된 둘
+        ^
         은은한 달빛에 감싸인 윔블던 숲 속의 깊은 밤. 손 끝을 스치는 살결과 향기에 취한 센슈얼한 밤12시를 느껴보세요.`,
     note: `
     -top: Lily of The Valley, White Lotus
+    ^
     -heart: White Musk, Powdery Notes
+    ^
     -base: Sensual Musk, Sandalwood, Benzoin
     `,
     perfumer: "Marianne Nawrocki Sabatier",
@@ -110,13 +127,18 @@ export const perfumeData = [
     category: "Perfume",
     price: "31,000원",
     src: "./images/shop/6am/m_6am.png",
-    desc: `&quot;The scent of enchanted grass created by spellbound forest spirits&quot;
+    desc: `The scent of enchanted grass created by spellbound forest spirits
+        ^
         Just before the sunrise, the mist of Wimbledon forest drifts along and greets you as you take a stroll. Feel the 6AM scent of the Wimbledon forest’s glistening morning dew and moist soil.
-        
-        &quot;숲의 정령이 만들어낸 중독적인 묘약, 신비로운 풀의 향취&quot;
+        ^
+        숲의 정령이 만들어낸 중독적인 묘약, 신비로운 풀의 향취
+        ^
         세상의 아침이 시작되기 전, 윔블던의 숲을 산책하는 당신에게 푸른 안개가 바람에 실려와 인사를 합니다. 이슬 머금은 잔디와 촉촉한 흙내음을 품은 윔블던의 새벽 6시를 느껴보세요.`,
-    note: `-top: Bergamot, Grapefruit, Anise Seeds
+    note: `
+        -top: Bergamot, Grapefruit, Anise Seeds
+        ^
         -heart: Cardamom, Clary Sage, Basil
+        ^
         -base: Wormwood, Cedarwood, Musk`,
     perfumer: "Aliénor Massenet",
     ingredients:
@@ -128,14 +150,17 @@ export const perfumeData = [
     category: "Perfume",
     price: "31,000원",
     src: "./images/shop/noon/m_noon.png",
-    desc: `&quot;The exquisite breeze around the lakeside bench filled with the harmonious scent of floral and aqua&quot;
+    desc: `The exquisite breeze around the lakeside bench filled with the harmonious scent of floral and aqua
+        ^
         Midday, when the sunlight sparkles on the surface of the lake, the bouquet of a full bloom iris and cool aqua scent under the tree shade fills up the air. Feel the scent of NOON at Wimbledon Park.
-
-        &quot;공원 호숫가 벤치에 불어오는 물과 꽃 향의 절묘한 조화&quot;
+        ^
+        공원 호숫가 벤치에 불어오는 물과 꽃 향의 절묘한 조화
         호수에 반짝이는 햇살 찬란한 낮 12시, 서늘한 나무그늘 아래 차가운 물 향과 햇살에 만개한 아이리스 꽃 향이 어우러진 윔블던 공원의 정오를 느껴보세요.`,
     note: `
         -top: Green Leaf, Aqua, Mint, Almond Blossom
+        ^
         -heart: Vetiver, Lavender, Lily of The Vally
+        ^
         -base: Orris(Iris), Cedar Wood, Amber, Musk
         `,
     perfumer: "Marianne Nawrocki Sabatier",
@@ -148,14 +173,18 @@ export const perfumeData = [
     category: "Perfume",
     price: "31,000원",
     src: "./images/shop/3pm/m_3pm.png",
-    desc: `&quot;The sweet scent of ripened fruits combined with lustrous grass under the beaming sunlight&quot;
+    desc: `The sweet scent of ripened fruits combined with lustrous grass under the beaming sunlight
+        ^
         While the forest’s morning dew dries away in the beaming afternoon sunlight, feel the scent of the fully blossomed flowers in the park come together with the freshness of lemon floating on a cup of black tea at 3PM in Wimbledon.
-        
-        &quot;햇살이 내리쬐는 푸른 잔디와 부드럽고 향긋한 과일의 향취&quot;
+        ^
+        햇살이 내리쬐는 푸른 잔디와 부드럽고 향긋한 과일의 향취
+        ^
         이슬 젖은 초록숲이 상큼한 햇살에 말라가는 윔블던의 오후. 공원에 만개한 꽃들의 향기와 홍차에 띄운 레몬 조각 향기가 어우러진 윔블던의 오후 3시를 느껴보세요.`,
     note: `
     -top: Bergamot, Rhubarb, Tangerine Lotus
+    ^
     -heart: Cedarwood, Jasmine, Gardenia
+    ^
     -base: White Musk, Vetiver, Orris
     `,
     perfumer: "Nathalie Benareau",
@@ -168,14 +197,18 @@ export const perfumeData = [
     category: "Perfume",
     price: "31,000원",
     src: "./images/shop/9pm/m_9pm.png",
-    desc: `&quot;The romance of a silent night in the woods&quot;
+    desc: `The romance of a silent night in the woods
+        ^
         The night in Wimbledon forest after dark. The whispers of love travel through the crackling bonfire. Feel the Wimbledon’s 9PM scent of the smokey bonfire smoothly blended with sweet vanilla.
-        
-        &quot;고요한 숲 속의 밤을 채운 사랑의 설레임&quot;
+        ^
+        고요한 숲 속의 밤을 채운 사랑의 설레임
+        ^
         어둠이 내린 윔블던 숲의 저녁. 모닥불 타는 소리 사이로 들려오는 연인들의 달콤한 속삭임. 바닐라의 달콤함과 숲 속 모닥불의 스모키함이 어우러진 윔블던의 저녁 9시를 느껴보세요.`,
     note: `
         -top: Pink Pepper, Orange flower, Clove , Marshmallow accord Lotus
+        ^
         -heart: Chestnut accord, Gaiac wood, Cade
+        ^
         -base: Sandalwood, Vanilla Accord, Peru balsam, Cashmeran
         `,
     perfumer: "Florian Gallo",
@@ -188,14 +221,18 @@ export const perfumeData = [
     category: "Perfume",
     price: "31,000원",
     src: "./images/shop/midnight/m_mid.png",
-    desc: `&quot;Deep in the soundless night, the scent of two lovers unite to create one&quot;
+    desc: `Deep in the soundless night, the scent of two lovers unite to create one
+        ^
         In the still of the night, the soft moonlight surrounds the Wimbledon forest. Feel the sensual MIDNIGHT through mesmerizing touch and scent.
-
-        &quot;모두가 잠든 깊은 밤, 서로의 향에 물들어 하나가 된 둘&quot;
+        ^
+        모두가 잠든 깊은 밤, 서로의 향에 물들어 하나가 된 둘
+        ^
         은은한 달빛에 감싸인 윔블던 숲 속의 깊은 밤. 손 끝을 스치는 살결과 향기에 취한 센슈얼한 밤12시를 느껴보세요.`,
     note: `
         -top: Lily of The Valley, White Lotus
+        ^
         -heart: White Musk, Powdery Notes
+        ^
         -base: Sensual Musk, Sandalwood, Benzoin
         `,
     perfumer: "Marianne Nawrocki Sabatier",
