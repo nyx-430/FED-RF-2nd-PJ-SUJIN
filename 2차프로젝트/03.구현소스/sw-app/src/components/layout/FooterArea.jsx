a81f9ac0a1fccee604dc849ad1f8f6311427dbb6
Sujin <9_ight@naver.com>
2024-06-27 15:23:55 +0900

    Update FooterArea.jsx

--- 2차프로젝트/03.구현소스/sw-app/src/components/layout/FooterArea.jsx
+++ 2차프로젝트/03.구현소스/sw-app/src/components/layout/FooterArea.jsx
@@ -15,13 +15,15 @@ export default function FooterArea() {
             <li>
               <a
                 target="_blank"
-                href="https://www.google.com/maps/@51.4390411,-0.2060724,3a,75y,177.6h,112.58t/data=!3m8!1e1!3m6!1sAF1QipNVOoAsKrqSsZsIzMvSN9i6Erg6gUBjSqRKqOCr!2e10!3e11!6shttps:%2F%2Flh5.googleusercontent.com%2Fp%2FAF1QipNVOoAsKrqSsZsIzMvSN9i6Erg6gUBjSqRKqOCr%3Dw203-h100-k-no-pi-0-ya49.77087-ro-0-fo100!7i8704!8i4352"
+                href="https://www.google.com/maps/@51.4390411,-0.2060724,3a,75y,177.6h,112.58t/data=!3m8!1e1!3m6!1sAF1QipNVOoAsKrqSsZsIzMvSN9i6Erg6gUBjSqRKqOCr!2e10!3e11!6shttps:%2F%2Flh5.googleusercontent.com%2Fp%2FAF1QipNVOoAsKrqSsZsIzMvSN9i6Erg6gUBjSqRKqOCr%3Dw900-h600-k-no-pi-22.58-ya119.6-ro0-fo90!7i8704!8i4352?coh=205410&entry=ttu"
               >
                 Experience SW19
               </a>
             </li>
             <li>
-              <a href="https://earth.google.com/static/9.3.118.1/app_min.html#search/Wimbledon+park/@51.4359333,-0.2101811,17.90890063a,628.90019534d,35y,287.91253015h,45t,0r/data=CnkaTxJJCiUweDQ4NzYwZjQ4ZmIyZTc2NTk6MHg4Yzc1NjBjODI2YTA5YzVhGWFekanMt0lAIQdgXts258q_Kg5XaW1ibGVkb24gcGFyaxgCIAEiJgokCX4y68BKLkpAEbFMe-6RLUpAGQqtfG7mlxNAIUJkXamOihNA">
+              <a 
+              target="_blank"
+              href="https://earth.google.com/web/@0,0,0a,22251752.77375655d,35y,0h,0t,0r">
                 SW19 in Google Earth
               </a>
             </li>
