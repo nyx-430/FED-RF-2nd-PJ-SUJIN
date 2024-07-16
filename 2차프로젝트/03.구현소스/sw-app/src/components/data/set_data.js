f4ded41a39e719d3296b26f47778d65fa80b407f
Sujin <9_ight@naver.com>
2024-07-16 13:04:28 +0900

    13:04

--- 2차프로젝트/03.구현소스/sw-app/src/components/data/set_data.js
+++ 2차프로젝트/03.구현소스/sw-app/src/components/data/set_data.js
@@ -1,6 +1,6 @@
 /// SW19 상품 데이터 ///
 
-// 세트 데이터 fullSet / hcSet / discovertSet
+// 세트 데이터 fullSet / hcSet / miniDuoSet / discovertSet
 
 const fullSet = [
   {
@@ -9,15 +9,39 @@ const fullSet = [
     price: "132,000원",
     src: "/images/shop/set/full_set_6am.png",
     detail: "/images/shop/6am/img_6am.png",
-    desc: ``,
+    desc: `The scent of enchanted grass created by spellbound forest spirits
+    ^
+    Just before the sunrise, the mist of Wimbledon forest drifts along and greets you as you take a stroll. Feel the 6AM scent of the Wimbledon forest’s glistening morning dew and moist soil.
+    ^
+    “숲의 정령이 만들어낸 중독적인 묘약, 신비로운 풀의 향취”
+    ^
+    세상의 아침이 시작되기 전, 윔블던의 숲을 산책하는 당신에게 푸른 안개가 바람에 실려와 인사를 합니다. 이슬 머금은 잔디와 촉촉한 흙내음을 품은 윔블던의 새벽 6시를 느껴보세요.`,
+    notice: `※ 향수와 핸드크림으로 구성된 기프트 세트입니다.
+    ^
+    ※ 상품 구매 시, 바디케어 샘플 키트가 함께 제공됩니다.(품절 시 제공불가)
+    ^
+    ※ 선물용 기프트백은 SHOP-ACC 메뉴에서 구매 가능합니다.
+    ^
+    ※ 30,000원 이상 구매 시, 무료 배송
+    ^
+    ※ 사용기한 : 제조일로부터 36개월`,
   },
   {
-    tit: "SW19 noon PERFUME & HAND CREAM DUO SET",
+    tit: "SW19 Noon PERFUME & HAND CREAM DUO SET",
     category: "Gift Set",
     price: "132,000원",
     src: "/images/shop/set/full_set_noon.png",
     detail: "/images/shop/noon/img_noon.png",
     desc: ``,
+   notice: `※ 향수와 핸드크림으로 구성된 기프트 세트입니다.
+    ^
+    ※ 상품 구매 시, 바디케어 샘플 키트가 함께 제공됩니다.(품절 시 제공불가)
+    ^
+    ※ 선물용 기프트백은 SHOP-ACC 메뉴에서 구매 가능합니다.
+    ^
+    ※ 30,000원 이상 구매 시, 무료 배송
+    ^
+    ※ 사용기한 : 제조일로부터 36개월`,
   },
   {
     tit: "SW19 3pm PERFUME & HAND CREAM DUO SET",
@@ -26,6 +50,15 @@ const fullSet = [
     src: "/images/shop/set/full_set_3pm.png",
     detail: "/images/shop/3pm/img_3pm.png",
     desc: ``,
+   notice: `※ 향수와 핸드크림으로 구성된 기프트 세트입니다.
+    ^
+    ※ 상품 구매 시, 바디케어 샘플 키트가 함께 제공됩니다.(품절 시 제공불가)
+    ^
+    ※ 선물용 기프트백은 SHOP-ACC 메뉴에서 구매 가능합니다.
+    ^
+    ※ 30,000원 이상 구매 시, 무료 배송
+    ^
+    ※ 사용기한 : 제조일로부터 36개월`,
   },
   {
     tit: "SW19 9pm PERFUME & HAND CREAM DUO SET",
@@ -34,6 +67,15 @@ const fullSet = [
     src: "/images/shop/set/full_set_9pm.png",
     detail: "/images/shop/9pm/img_9pm.png",
     desc: ``,
+   notice: `※ 향수와 핸드크림으로 구성된 기프트 세트입니다.
+    ^
+    ※ 상품 구매 시, 바디케어 샘플 키트가 함께 제공됩니다.(품절 시 제공불가)
+    ^
+    ※ 선물용 기프트백은 SHOP-ACC 메뉴에서 구매 가능합니다.
+    ^
+    ※ 30,000원 이상 구매 시, 무료 배송
+    ^
+    ※ 사용기한 : 제조일로부터 36개월`,
   },
   {
     tit: "SW19 Midnight PERFUME & HAND CREAM DUO SET",
@@ -42,35 +84,106 @@ const fullSet = [
     src: "/images/shop/set/full_set_mid.png",
     detail: "/images/shop/midnight/img_midnight.png",
     desc: ``,
+   notice: `※ 향수와 핸드크림으로 구성된 기프트 세트입니다.
+    ^
+    ※ 상품 구매 시, 바디케어 샘플 키트가 함께 제공됩니다.(품절 시 제공불가)
+    ^
+    ※ 선물용 기프트백은 SHOP-ACC 메뉴에서 구매 가능합니다.
+    ^
+    ※ 30,000원 이상 구매 시, 무료 배송
+    ^
+    ※ 사용기한 : 제조일로부터 36개월`,
   },
-  {
-    tit: "SW19 DISCOVERY SET (8ml*5)",
-    category: "Gift Set",
-    price: "118,000원",
-    src: "/images/shop/set/full_set_mid.png",
-    detail: "/images/shop/midnight/img_midnight.png",
-    desc: ``,
-  },
-]
+];
 
 const hcSet = [
-    {
-        tit: "SW19 HAND CREAM TRIO SET",
-        category: "Gift Set",
-        price: "69,000원",
-        src: "/images/shop/set/handcream_trio_set.png",
-        detail: "handcream_trio_set_detail.png",
-        desc: ``,
-    },
-    {
-        tit: "SW19 HAND CREAM MINI SET (10ml*5)",
-        category: "Gift Set",
-        price: "30,000원",
-        src: "/images/shop/set/handcream_mini_set",
-        detail: "handcream_trio_set_detail.png",
-        desc: ``,
-    },
-]
+  {
+    tit: "SW19 HAND CREAM TRIO SET",
+    category: "Gift Set",
+    price: "69,000원",
+    src: "/images/shop/set/handcream_trio_set.png",
+    detail: "handcream_trio_set_detail.png",
+    desc: `The SW19 perfume-scented hand cream is enriched with silky powder and mild botanical moisturizing ingredients to nourish and moisturize hands without sticky after-feel.
+    ^
+    SW19 퍼퓸 향을 그대로 담은 핸드크림은 실키 파우더가 함유되어 끈적임 없이 매끄럽게 피부결을 정돈하며, 비건 포뮬러로 만들어진 순한 식물성 보습 성분이 피부를 산뜻하고 건강하게 유지합니다.
+    ^
+    Ingredients certified by the EVE VEGAN® MARK`,
+    notice: `※ 3종의 핸드크림으로 구성된 기프트 세트입니다.
+    ^
+    ※ 상품 구매 시, 바디케어 샘플 키트가 함께 제공됩니다.(품절 시 제공불가)
+    ^
+    ※ 선물용 기프트백은 SHOP-ACC 메뉴에서 구매 가능합니다.
+    ^
+    ※ 30,000원 이상 구매 시, 무료 배송
+    ^
+    ※ 사용기한 : 제조일로부터 36개월`,
+  },
+  {
+    tit: "SW19 HAND CREAM MINI SET (10ml*5)",
+    category: "Gift Set",
+    price: "30,000원",
+    src: "/images/shop/set/handcream_mini_set",
+    detail: "handcream_trio_set_detail.png",
+    desc: `The SW19 perfume-scented hand cream is enriched with silky powder and mild botanical moisturizing ingredients to nourish and moisturize hands without sticky after-feel.
+    ^
+    SW19 퍼퓸 향을 그대로 담은 핸드크림은 실키 파우더가 함유되어 끈적임 없이 매끄럽게 피부결을 정돈하며, 비건 포뮬러로 만들어진 순한 식물성 보습 성분이 피부를 산뜻하고 건강하게 유지합니다.
+    ^
+    Ingredients certified by the EVE VEGAN® MARK
+    `,
+    notice: `※ 5종의 미니 핸드크림으로 구성된 기프트 세트입니다.
+    ^
+    ※ 상품 구매 시, 바디케어 샘플 키트가 함께 제공됩니다.(품절 시 제공불가)`,
+  },
+];
+
+const miniDuoSet = [
+  {
+    tit: "SW19 MIDNIGHT MINI DUO SET",
+    category: "Gift Set",
+    price: "54,000원",
+    src: "/images/shop/set/mini_duo_set_mid.png",
+    detail: "/images/shop/set/mini_duo_set_mid_detail.png",
+    desc: `Deep in the soundless night, the scent of two lovers unite to create one
+    ^
+    In the still of the night, the soft moonlight surrounds the Wimbledon forest. Feel the sensual MIDNIGHT through mesmerizing touch and scent.
+    ^
+    모두가 잠든 깊은 밤, 서로의 향에 물들어 하나가 된 둘
+    ^
+    은은한 달빛에 감싸인 윔블던 숲 속의 깊은 밤. 손 끝을 스치는 살결과 향기에 취한 센슈얼한 밤12시를 느껴보세요.`,
+    notice: `※ 향수와 핸드크림으로 구성된 기프트 세트입니다.
+    ^
+    ※ 상품 구매 시, 바디케어 샘플 키트가 함께 제공됩니다.(품절 시 제공불가)
+    ^
+    ※ 선물용 기프트백은 SHOP-ACC 메뉴에서 구매 가능합니다.
+    ^
+    ※ 30,000원 이상 구매 시, 무료 배송
+    ^
+    ※ 사용기한 : 제조일로부터 36개월`,
+  },
+  {
+    tit: "SW19 3PM MINI DUO SET",
+    category: "Gift Set",
+    price: "54,000원",
+    src: "/images/shop/set/mini_duo_set_mid.png",
+    detail: "/images/shop/set/mini_duo_set_3pm_detail.png",
+    desc: `Deep in the soundless night, the scent of two lovers unite to create one
+    ^
+    In the still of the night, the soft moonlight surrounds the Wimbledon forest. Feel the sensual MIDNIGHT through mesmerizing touch and scent.
+    ^
+    모두가 잠든 깊은 밤, 서로의 향에 물들어 하나가 된 둘
+    ^
+    은은한 달빛에 감싸인 윔블던 숲 속의 깊은 밤. 손 끝을 스치는 살결과 향기에 취한 센슈얼한 밤12시를 느껴보세요.`,
+    notice: `※ 향수와 핸드크림으로 구성된 기프트 세트입니다.
+    ^
+    ※ 상품 구매 시, 바디케어 샘플 키트가 함께 제공됩니다.(품절 시 제공불가)
+    ^
+    ※ 선물용 기프트백은 SHOP-ACC 메뉴에서 구매 가능합니다.
+    ^
+    ※ 30,000원 이상 구매 시, 무료 배송
+    ^
+    ※ 사용기한 : 제조일로부터 36개월`,
+  },
+];
 
 const discovertSet = [
   {
@@ -81,6 +194,38 @@ const discovertSet = [
     detail: "/images/shop/set/discovery_set_detail.png",
     desc: `Explore full collection of SW19 Eau de parfum Discovery set which was inspired by green forest of the Wimbledon, London SW19 that changes over time.
     ^
-    시간에 따라 변화하는 런던 윔블던 숲과 공원의 향을 담은 SW19의 오드 퍼퓸 컬렉션 다섯 가지 향을 모두 경험할 수 있는 디스커버리세트 입니다.`,
+    시간에 따라 변화하는 런던 윔블던 숲과 공원의 향을 담은 SW19의 오드 퍼퓸 컬렉션 다섯 가지 향을 모두 경험할 수 있는 디스커버리세트 입니다.
+    ^
+    Ingredients certified by the EVE VEGAN® MARK`,
+    notice: `※ 향수 5종으로 구성된 세트입니다.
+    ^
+    ※ 해당 제품은 수작업으로 제작되는 유리 보틀의 내측 용량 편차로 인해 개별 용량에 차이가 있어 보일 수 있으나, 제조 시 동일 용량이 주입되는 점 참고 부탁드립니다.
+    ^
+    ※ 상품 구매 시, 바디케어 샘플 키트가 함께 제공됩니다.(품절 시 제공불가)
+    ^
+    ※ 선물용 기프트백은 SHOP-ACC 메뉴에서 구매 가능합니다.
+    ^
+    ※ 30,000원 이상 구매 시, 무료 배송
+    ^
+    ※ 사용기한 : 제조일로부터 36개월`,
   },
-]
+  {
+    tit: "SW19 MINI DISCOVERY SET",
+    category: "Gift Set",
+    price: "30,000원",
+    src: "/images/shop/set/discovery_set.png",
+    detail: "/images/shop/set/discovery_set_detail.png",
+    desc: `Explore full collection of SW19 Eau de parfum Mini Discovery set which was inspired by green forest of the Wimbledon, London SW19 that changes over time.
+    ^
+    시간에 따라 변화하는 런던 윔블던 숲과 공원의 향을 담은 SW19의 오 드 퍼퓸 컬렉션 다섯 가지 향을 모두 경험할 수 있는 미니 디스커버리 세트 입니다.`,
+    notice: `※ 향수 2ml 5종으로 구성된 세트입니다.
+    ^
+    ※ 상품 구매 시, 바디케어 샘플 키트가 함께 제공됩니다.(품절 시 제공불가)
+    ^
+    ※ 선물용 기프트백은 SHOP-ACC 메뉴에서 구매 가능합니다.
+    ^
+    ※ 30,000원 이상 구매 시, 무료 배송
+    ^
+    ※ 사용기한 : 제조일로부터 36개월`,
+  },
+];
