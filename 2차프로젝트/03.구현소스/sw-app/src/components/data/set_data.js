c9e4a3d94518d91f26ea844facd9fcc8f6ec2f4a
Sujin <9_ight@naver.com>
2024-07-16 11:57:01 +0900

    add

---
+++ 2차프로젝트/03.구현소스/sw-app/src/components/data/set_data.js
@@ -0,0 +1,58 @@
+/// SW19 상품 데이터 ///
+
+// 세트 데이터 giftSetData /
+
+const fullSetData = [
+  {
+    tit: "SW19 6am PERFUME & HAND CREAM DUO SET",
+    category: "Gift Set",
+    price: "132,000원",
+    src: "/images/shop/set/full_set_6am.png",
+    img: "/images/shop/6am/img_6am.png",
+  },
+  {
+    tit: "SW19 noon PERFUME & HAND CREAM DUO SET",
+    category: "Gift Set",
+    price: "132,000원",
+    src: "/images/shop/set/full_set_noon.png",
+    img: "/images/shop/noon/img_noon.png",
+  },
+  {
+    tit: "SW19 3pm PERFUME & HAND CREAM DUO SET",
+    category: "Gift Set",
+    price: "132,000원",
+    src: "/images/shop/set/full_set_3pm.png",
+    img: "/images/shop/3pm/img_3pm.png",
+  },
+  {
+    tit: "SW19 9pm PERFUME & HAND CREAM DUO SET",
+    category: "Gift Set",
+    price: "132,000원",
+    src: "/images/shop/set/full_set_9pm.png",
+    img: "/images/shop/9pm/img_9pm.png",
+  },
+  {
+    tit: "SW19 Midnight PERFUME & HAND CREAM DUO SET",
+    category: "Gift Set",
+    price: "132,000원",
+    src: "/images/shop/set/full_set_mid.png",
+    img: "/images/shop/midnight/img_img_midnight.png",
+  },
+  {
+    tit: "SW19 DISCOVERY SET (8ml*5)",
+    category: "Gift Set",
+    price: "118,000원",
+    src: "/images/shop/set/full_set_mid.png",
+    img: "/images/shop/midnight/img_img_midnight.png",
+  },
+]
+
+const hcSetData = [
+    {
+        tit: "SW19 HAND CREAM TRIO SET",
+        category: "Gift Set",
+        price: "69,000원",
+        src: "/images/shop/set/handcream_trio_set.png",
+        img: "handcream_trio_set_detail.png",
+    },
+]
