f89e75f37372cdbccc076440f71ee46f51835bfa
Sujin <9_ight@naver.com>
2024-07-16 12:20:06 +0900

    add

--- 2차프로젝트/03.구현소스/sw-app/src/components/data/set_data.js
+++ 2차프로젝트/03.구현소스/sw-app/src/components/data/set_data.js
@@ -1,14 +1,15 @@
 /// SW19 상품 데이터 ///
 
-// 세트 데이터 giftSetData /
+// 세트 데이터 fullSet / hcSet / discovertSet
 
-const fullSetData = [
+const fullSet = [
   {
     tit: "SW19 6am PERFUME & HAND CREAM DUO SET",
     category: "Gift Set",
     price: "132,000원",
     src: "/images/shop/set/full_set_6am.png",
     detail: "/images/shop/6am/img_6am.png",
+    desc: ``,
   },
   {
     tit: "SW19 noon PERFUME & HAND CREAM DUO SET",
@@ -16,6 +17,7 @@ const fullSetData = [
     price: "132,000원",
     src: "/images/shop/set/full_set_noon.png",
     detail: "/images/shop/noon/img_noon.png",
+    desc: ``,
   },
   {
     tit: "SW19 3pm PERFUME & HAND CREAM DUO SET",
@@ -23,6 +25,7 @@ const fullSetData = [
     price: "132,000원",
     src: "/images/shop/set/full_set_3pm.png",
     detail: "/images/shop/3pm/img_3pm.png",
+    desc: ``,
   },
   {
     tit: "SW19 9pm PERFUME & HAND CREAM DUO SET",
@@ -30,6 +33,7 @@ const fullSetData = [
     price: "132,000원",
     src: "/images/shop/set/full_set_9pm.png",
     detail: "/images/shop/9pm/img_9pm.png",
+    desc: ``,
   },
   {
     tit: "SW19 Midnight PERFUME & HAND CREAM DUO SET",
@@ -37,6 +41,7 @@ const fullSetData = [
     price: "132,000원",
     src: "/images/shop/set/full_set_mid.png",
     detail: "/images/shop/midnight/img_midnight.png",
+    desc: ``,
   },
   {
     tit: "SW19 DISCOVERY SET (8ml*5)",
@@ -44,16 +49,18 @@ const fullSetData = [
     price: "118,000원",
     src: "/images/shop/set/full_set_mid.png",
     detail: "/images/shop/midnight/img_midnight.png",
+    desc: ``,
   },
 ]
 
-const hcSetData = [
+const hcSet = [
     {
         tit: "SW19 HAND CREAM TRIO SET",
         category: "Gift Set",
         price: "69,000원",
         src: "/images/shop/set/handcream_trio_set.png",
         detail: "handcream_trio_set_detail.png",
+        desc: ``,
     },
     {
         tit: "SW19 HAND CREAM MINI SET (10ml*5)",
@@ -61,5 +68,19 @@ const hcSetData = [
         price: "30,000원",
         src: "/images/shop/set/handcream_mini_set",
         detail: "handcream_trio_set_detail.png",
+        desc: ``,
     },
+]
+
+const discovertSet = [
+  {
+    tit: "SW19 DISCOVERY SET (8ml*5)",
+    category: "Gift Set",
+    price: "118,000원",
+    src: "/images/shop/set/discovery_set.png",
+    detail: "/images/shop/set/discovery_set_detail.png",
+    desc: `Explore full collection of SW19 Eau de parfum Discovery set which was inspired by green forest of the Wimbledon, London SW19 that changes over time.
+    ^
+    시간에 따라 변화하는 런던 윔블던 숲과 공원의 향을 담은 SW19의 오드 퍼퓸 컬렉션 다섯 가지 향을 모두 경험할 수 있는 디스커버리세트 입니다.`,
+  },
 ]
