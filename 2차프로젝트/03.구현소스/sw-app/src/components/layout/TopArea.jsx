7f664086e6e59b048b307fb5ca2c70400094938a
Sujin <9_ight@naver.com>
2024-07-24 13:20:36 +0900

    13:20

--- 2차프로젝트/03.구현소스/sw-app/src/components/layout/TopArea.jsx
+++ 2차프로젝트/03.구현소스/sw-app/src/components/layout/TopArea.jsx
@@ -11,7 +11,6 @@ import $ from "jquery";
 // GNB 데이터
 import { menu } from "../data/gnb";
 
-
 // 폰트어썸
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faRightToBracket } from "@fortawesome/free-solid-svg-icons";
@@ -22,187 +21,188 @@ import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 // 헤더 탑메뉴 CSS
 import "../../css/top_area.scss";
 
-export const TopArea = memo(({ loginMsg, loginSts, logoutFn, goPage, showTab }) => {
-  // 전달값
-  // 1. loginMsg - 로그인 메시지 변수
-  // 2. loginSts - 로그인 상태 변수
-  // 3. logoutFn - 로그아웃 함수
+export const TopArea = memo(
+  ({ loginMsg, loginSts, logoutFn, goPage, showTab }) => {
+    // 전달값
+    // 1. loginMsg - 로그인 메시지 변수
+    // 2. loginSts - 로그인 상태 변수
+    // 3. logoutFn - 로그아웃 함수
 
-  // 쇼핑탭 버튼 클래스 on 상태관리변수 -> 수정 필요
-  // const [visible, setVisible] = useState(false);
-  // const ref = useRef(null);
+    // 쇼핑탭 버튼 클래스 on 상태관리변수 -> 수정 필요
+    // const [visible, setVisible] = useState(false);
+    // const ref = useRef(null);
 
-  const location = useLocation();
+    const location = useLocation();
 
-  ///////////////////////////////////////////////
-  ////////////////// [ 함수 ] //////////////////
-  // 이동 함수
-  const goNav = useNavigate();
+    ///////////////////////////////////////////////
+    ////////////////// [ 함수 ] //////////////////
+    // 이동 함수
+    const goNav = useNavigate();
 
-  // 메뉴 버튼 클래스 on 함수
-  const addOn = () => {
-    document.querySelector(".menu-button").classList.toggle("on");
-    document.querySelector(".menu-open").classList.toggle("on");
-  }; ////// addOn //////
+    // 메뉴 버튼 클래스 on 함수
+    const addOn = () => {
+      document.querySelector(".menu-button").classList.toggle("on");
+      document.querySelector(".menu-open").classList.toggle("on");
+    }; ////// addOn //////
 
-    
-  ///////////////////////////////////////////////
-  
-  // 랜더링 구역 ////////////////////////
-  useEffect(() => {
-    // 메뉴 버튼 클래스 on 이벤트
-    document.querySelector(".menu-button").classList.remove("on");
-    document.querySelector(".menu-open").classList.remove("on");
+    ///////////////////////////////////////////////
 
-    // 쇼핑탭 버튼 이벤트
-    // document.addEventListener('mousedown', showTab);
-    // return () => {
-    //   document.removeEventListener('mousedown', showTab);
-    // };
-  }, [location]); ////////////////////////
+    // 랜더링 구역 ////////////////////////
+    useEffect(() => {
+      // 메뉴 버튼 클래스 on 이벤트
+      document.querySelector(".menu-button").classList.remove("on");
+      document.querySelector(".menu-open").classList.remove("on");
 
-  // 검색 관련 함수들 //////////////////////
-  // 검색창에 엔터키 누르면 검색 함수 호출
-  const enterKey = (e) => {
-    // console.log(e.key,e.keyCode);
-    // e.keyCode는 숫자, e.key문자로 리턴함
+      // 쇼핑탭 버튼 이벤트
+      // document.addEventListener('mousedown', showTab);
+      // return () => {
+      //   document.removeEventListener('mousedown', showTab);
+      // };
+    }, [location]); ////////////////////////
 
-    if (e.key == "Enter") {
-      // 입력창의 입력값 읽어오기 : val() 사용
-      let txt = $(e.target).val().trim();
-      console.log(txt);
+    // 검색 관련 함수들 //////////////////////
+    // 검색창에 엔터키 누르면 검색 함수 호출
+    const enterKey = (e) => {
+      // console.log(e.key,e.keyCode);
+      // e.keyCode는 숫자, e.key문자로 리턴함
 
-      // 빈 값이 아니면 검색 함수 호출 (검색어 전달!)
-      if (txt != "") {
-        // 입력창 비우고 부모박스 닫기
-        $(e.target).val("");
+      if (e.key == "Enter") {
+        // 입력창의 입력값 읽어오기 : val() 사용
+        let txt = $(e.target).val().trim();
+        console.log(txt);
 
-        // 검색 보내기
-        goSearch(txt);
+        // 빈 값이 아니면 검색 함수 호출 (검색어 전달!)
+        if (txt != "") {
+          // 입력창 비우고 부모박스 닫기
+          $(e.target).val("");
+
+          // 검색 보내기
+          goSearch(txt);
+        } /// if ///
       } /// if ///
-    } /// if ///
-  }; ////// enterKey //////
+    }; ////// enterKey //////
 
-  // 3. 검색 페이지로 검색어와 함께 이동하기 함수
-  const goSearch = (txt) => {
-    console.log("검색 시작!");
+    // 3. 검색 페이지로 검색어와 함께 이동하기 함수
+    const goSearch = (txt) => {
+      console.log("검색 시작!");
 
-    // 라우터 이동 함수로 이동하기
-    // 네비게이트 메서드("라우터 주소",{state:{보낼 객체}})
-    goNav("/search", { state: { keyword: txt } });
-  }; ////// goSearch //////
+      // 라우터 이동 함수로 이동하기
+      // 네비게이트 메서드("라우터 주소",{state:{보낼 객체}})
+      goNav("/search", { state: { keyword: txt } });
+    }; ////// goSearch //////
 
-  // 코드 리턴 구역 ////////////////////////
-  return (
-    <>
-      <header id="gnb">
-        <div className="menu">
-          <div className="lines">
-            <a href="/">
-              <span id="logo" className="line">
-                SW19
-              </span>
-            </a>
-            <p className="line">
-              A transitional scent of the green forest | of Wimbledon, London{" "}
-              <br />
-              SW19 | that changes over time
-            </p>
-          </div>
-          {/* 메뉴 아이콘 박스 */}
-          <div className="menu-box">
-            {/* <!-- 햄버거 버튼 --> */}
-            <div className="menu-button" onClick={addOn}>
-              <span></span>
-              <span></span>
-              <span></span>
+    // 코드 리턴 구역 ////////////////////////
+    return (
+      <>
+        <header id="gnb">
+          <div className="menu">
+            <div className="lines">
+              <Link to="/">
+                <span id="logo" className="line">
+                  SW19
+                </span>
+              </Link>
+              <p className="line">
+                A transitional scent of the green forest | of Wimbledon, London{" "}
+                <br />
+                SW19 | that changes over time
+              </p>
             </div>
-            {/* <!-- 하단 아이콘 박스 --> */}
-            <div className="icon-box">
-              <ul>
-                <li className="menu-buttom">
-                  {/* 로그인 */}
-                  <Link to="/login">
-                    <FontAwesomeIcon icon={faRightToBracket} />
-                  </Link>
-                </li>
-                <li className="menu-buttom">
-                  {/* 회원가입 */}
-                  <Link to="/member">
-                    <FontAwesomeIcon icon={faUser} />
-                  </Link>
-                </li>
-                {/* 장바구니 */}
-                <li className="menu-buttom cart"
-                // onClick={()=>{setVisible(true)}}
-                onClick={showTab}
-                style={{cursor: "pointer"}}
-                >
-                  <FontAwesomeIcon icon={faBagShopping} />
-                </li>
-              </ul>
+            {/* 메뉴 아이콘 박스 */}
+            <div className="menu-box">
+              {/* <!-- 햄버거 버튼 --> */}
+              <div className="menu-button" onClick={addOn}>
+                <span></span>
+                <span></span>
+                <span></span>
+              </div>
+              {/* <!-- 하단 아이콘 박스 --> */}
+              <div className="icon-box">
+                <ul>
+                  <li className="menu-buttom">
+                    {/* 로그인 */}
+                    <Link to="/login">
+                      <FontAwesomeIcon icon={faRightToBracket} />
+                    </Link>
+                  </li>
+                  <li className="menu-buttom">
+                    {/* 회원가입 */}
+                    <Link to="/member">
+                      <FontAwesomeIcon icon={faUser} />
+                    </Link>
+                  </li>
+                  {/* 장바구니 */}
+                  <li
+                    className="menu-buttom cart"
+                    // onClick={()=>{setVisible(true)}}
+                    onClick={showTab}
+                    style={{ cursor: "pointer" }}
+                  >
+                    <FontAwesomeIcon icon={faBagShopping} />
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
-        </div>
-        {/* 사이드 메뉴 - 카테고리 */}
-        <aside className="menu-open">
-          {/* 검색 입력 박스 */}
-          <div className="search-bar">
-            <input
-              type="text"
-              placeholder="Search"
-              name="schinGnb"
-              id="schinGnb"
-              onKeyUp={enterKey}
-            />
-            <button className="schbtn">
-              <FontAwesomeIcon
-                icon={faMagnifyingGlass}
-                className="schbtnGnb"
-                title="Open search"
-                onClick={(e) => {
-                  // 검색어 읽기
-                  let stxt = e.currentTarget.nextElementSibling.value;
-                  if (stxt.trim() != "") {
-                    // 검색하기
-                    goSearch(stxt);
-                  } /// if ///
-                  else {
-                    // 검색어 비었을 때 메시지
-                    alert("Please enter a search term!");
-                  }
-                }}
+          {/* 사이드 메뉴 - 카테고리 */}
+          <aside className="menu-open">
+            {/* 검색 입력 박스 */}
+            <div className="search-bar">
+              <input
+                type="text"
+                placeholder="Search"
+                name="schinGnb"
+                id="schinGnb"
+                onKeyUp={enterKey}
               />
-            </button>
-          </div>
-          <ul>
-            {menu.map((v, i) => (
-              <li key={i}>
-                {v.sub ? v.txt : <Link to={v.link}>{v.txt}</Link>}
-                {v.sub && (
-                  <div className="smenu">
-                    <ol>
-                      {v.sub.map((v, i) => (
-                        <li key={i}>
-                          <Link to={v.link} state={{ category: v.txt }}>
-                            - {v.txt}
-                          </Link>
-                        </li>
-                      ))}
-                    </ol>
-                  </div>
-                )}
-              </li>
-            ))}
-          </ul>
-        </aside>
-        {/* 사이드 메뉴 - 쇼핑탭 */}
-        {/* <aside 
+              <button className="schbtn">
+                <FontAwesomeIcon
+                  icon={faMagnifyingGlass}
+                  className="schbtnGnb"
+                  title="Open search"
+                  onClick={(e) => {
+                    // 검색어 읽기
+                    let stxt = e.currentTarget.nextElementSibling.value;
+                    if (stxt.trim() != "") {
+                      // 검색하기
+                      goSearch(stxt);
+                    } /// if ///
+                    else {
+                      // 검색어 비었을 때 메시지
+                      alert("Please enter a search term!");
+                    }
+                  }}
+                />
+              </button>
+            </div>
+            <ul>
+              {menu.map((v, i) => (
+                <li key={i}>
+                  {v.sub ? v.txt : <Link to={v.link}>{v.txt}</Link>}
+                  {v.sub && (
+                    <div className="smenu">
+                      <ol>
+                        {v.sub.map((v, i) => (
+                          <li key={i}>
+                            <Link to={v.link} state={{ category: v.txt }}>
+                              - {v.txt}
+                            </Link>
+                          </li>
+                        ))}
+                      </ol>
+                    </div>
+                  )}
+                </li>
+              ))}
+            </ul>
+          </aside>
+          {/* 사이드 메뉴 - 쇼핑탭 */}
+          {/* <aside 
         ref={ref}
         className={`shopping-tab ${visible ? "on" : ""}`}
         > */}
-        
-      </header>
-    </>
-  );
-}); /////////// TopArea ///////////
+        </header>
+      </>
+    );
+  }
+); /////////// TopArea ///////////
